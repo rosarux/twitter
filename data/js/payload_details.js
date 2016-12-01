2e09d8e7acb94a171458457c264ab1d88e8765e5
rosarux <seungwoo.ha@gmail.com>
2016-12-01 09:21:35 +0900

    Added 20 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 14815,
-	"created_at": "Wed Nov 30 00:21:36 +0000 2016",
+	"tweets": 14835,
+	"created_at": "Thu Dec 01 00:21:34 +0000 2016",
 	"lang": "ja"
 }
--- data/js/tweet_index.js
+++ data/js/tweet_index.js
@@ -1,9 +1,16 @@
 var tweet_index = [
 	{
+		"file_name": "data/js/tweets/2016_12.js",
+		"year": 2016,
+		"var_name": "tweets_2016_12",
+		"tweet_count": 3,
+		"month": 12
+	},
+	{
 		"file_name": "data/js/tweets/2016_11.js",
 		"year": 2016,
 		"var_name": "tweets_2016_11",
-		"tweet_count": 252,
+		"tweet_count": 269,
 		"month": 11
 	},
 	{
