54ddc88f4d436db5c7ba7f43f68bd5c476340106
rosarux <seungwoo.ha@gmail.com>
2018-06-10 09:21:35 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 17106,
-	"created_at": "Tue Jun 05 00:21:34 +0000 2018",
+	"tweets": 17107,
+	"created_at": "Sun Jun 10 00:21:34 +0000 2018",
 	"lang": "ja"
 }
--- data/js/tweets/2018_06.js
+++ data/js/tweets/2018_06.js
@@ -1,6 +1,385 @@
 Grailbird.data.tweets_2018_06 = 
 [
 	{
+		"created_at": "Sat Jun 09 05:39:43 +0000 2018",
+		"id": 1005323536238403600,
+		"id_str": "1005323536238403584",
+		"text": "RT @super_elm: スラダン新装版流川ここらへん？\n#SlamDunk https://t.co/11LtiX2wPu",
+		"truncated": false,
+		"entities": {
+			"hashtags": [
+				{
+					"text": "SlamDunk",
+					"indices": [
+						31,
+						40
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "super_elm",
+					"name": "中野健太（スーパーエレメント）",
+					"id": 97894822,
+					"id_str": "97894822",
+					"indices": [
+						3,
+						13
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1002690597579251700,
+					"id_str": "1002690597579251712",
+					"indices": [
+						41,
+						64
+					],
+					"media_url": "http://pbs.twimg.com/media/DepFyThV4AAycPZ.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DepFyThV4AAycPZ.jpg",
+					"url": "https://t.co/11LtiX2wPu",
+					"display_url": "pic.twitter.com/11LtiX2wPu",
+					"expanded_url": "https://twitter.com/super_elm/status/1002690603950342144/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1024,
+							"h": 768,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1024,
+							"h": 768,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 1002690603950342100,
+					"source_status_id_str": "1002690603950342144",
+					"source_user_id": 97894822,
+					"source_user_id_str": "97894822"
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1002690597579251700,
+					"id_str": "1002690597579251712",
+					"indices": [
+						41,
+						64
+					],
+					"media_url": "http://pbs.twimg.com/media/DepFyThV4AAycPZ.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DepFyThV4AAycPZ.jpg",
+					"url": "https://t.co/11LtiX2wPu",
+					"display_url": "pic.twitter.com/11LtiX2wPu",
+					"expanded_url": "https://twitter.com/super_elm/status/1002690603950342144/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1024,
+							"h": 768,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1024,
+							"h": 768,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 1002690603950342100,
+					"source_status_id_str": "1002690603950342144",
+					"source_user_id": 97894822,
+					"source_user_id_str": "97894822"
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 94077994,
+			"id_str": "94077994",
+			"name": "縄",
+			"screen_name": "kawanoboru",
+			"location": "마음 만은 언제나 오키나와",
+			"description": "沖縄戦後史を勉強してた、ソウルに住んでいる普通の会社員 오키나와 전후사를 공부한 적이 있는, 서울에 살고 있는 평범한 회사원",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 594,
+			"friends_count": 494,
+			"listed_count": 22,
+			"created_at": "Wed Dec 02 12:19:58 +0000 2009",
+			"favourites_count": 2000,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 16754,
+			"lang": "ja",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": true,
+			"profile_background_color": "B2DFDA",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme13/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme13/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/983561990789521409/yByV3Crc_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/983561990789521409/yByV3Crc_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/94077994/1441838938",
+			"profile_link_color": "93A644",
+			"profile_sidebar_border_color": "EEEEEE",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"retweeted_status": {
+			"created_at": "Fri Jun 01 23:17:23 +0000 2018",
+			"id": 1002690603950342100,
+			"id_str": "1002690603950342144",
+			"text": "スラダン新装版流川ここらへん？\n#SlamDunk https://t.co/11LtiX2wPu",
+			"truncated": false,
+			"entities": {
+				"hashtags": [
+					{
+						"text": "SlamDunk",
+						"indices": [
+							16,
+							25
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 1002690597579251700,
+						"id_str": "1002690597579251712",
+						"indices": [
+							26,
+							49
+						],
+						"media_url": "http://pbs.twimg.com/media/DepFyThV4AAycPZ.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DepFyThV4AAycPZ.jpg",
+						"url": "https://t.co/11LtiX2wPu",
+						"display_url": "pic.twitter.com/11LtiX2wPu",
+						"expanded_url": "https://twitter.com/super_elm/status/1002690603950342144/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 1024,
+								"h": 768,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1024,
+								"h": 768,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 510,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1002690597579251700,
+						"id_str": "1002690597579251712",
+						"indices": [
+							26,
+							49
+						],
+						"media_url": "http://pbs.twimg.com/media/DepFyThV4AAycPZ.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DepFyThV4AAycPZ.jpg",
+						"url": "https://t.co/11LtiX2wPu",
+						"display_url": "pic.twitter.com/11LtiX2wPu",
+						"expanded_url": "https://twitter.com/super_elm/status/1002690603950342144/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 1024,
+								"h": 768,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1024,
+								"h": 768,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 510,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 97894822,
+				"id_str": "97894822",
+				"name": "中野健太（スーパーエレメント）",
+				"screen_name": "super_elm",
+				"location": "言うほどディグってない",
+				"description": "Superelement グラフィックデザイナー。NYTDC会員。デザインネタをディグって血肉にします。note→https://t.co/NUriege3X9  Instagram →https://t.co/lSMEpNxhSR お仕事のご依頼はお気軽にDMください。",
+				"url": "https://t.co/WpchudUS2Z",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/WpchudUS2Z",
+								"expanded_url": "http://super-elm.com",
+								"display_url": "super-elm.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": [
+							{
+								"url": "https://t.co/NUriege3X9",
+								"expanded_url": "http://note.mu/superelement",
+								"display_url": "note.mu/superelement",
+								"indices": [
+									57,
+									80
+								]
+							},
+							{
+								"url": "https://t.co/lSMEpNxhSR",
+								"expanded_url": "http://Instagram.com/kentanakano/",
+								"display_url": "Instagram.com/kentanakano/",
+								"indices": [
+									93,
+									116
+								]
+							}
+						]
+					}
+				},
+				"protected": false,
+				"followers_count": 152,
+				"friends_count": 175,
+				"listed_count": 3,
+				"created_at": "Sat Dec 19 13:05:23 +0000 2009",
+				"favourites_count": 2022,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 772,
+				"lang": "ja",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "C0DEED",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": true,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/956127290793930752/8r_Jj9lR_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/956127290793930752/8r_Jj9lR_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/97894822/1519717631",
+				"profile_link_color": "0084B4",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 9285,
+			"favorite_count": 19174,
+			"favorited": false,
+			"retweeted": true,
+			"possibly_sensitive": false,
+			"lang": "ja"
+		},
+		"is_quote_status": false,
+		"retweet_count": 9285,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ja"
+	},
+	{
 		"created_at": "Mon Jun 04 10:57:20 +0000 2018",
 		"id": 1003591528017682400,
 		"id_str": "1003591528017682433",
