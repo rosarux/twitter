cd3f106375b2dd13b3e48df9ed1054a5b87f01f9
rosarux <seungwoo.ha@gmail.com>
2018-11-16 09:21:35 +0900

    Added 3 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 17890,
-	"created_at": "Thu Nov 15 00:21:35 +0000 2018",
+	"tweets": 17893,
+	"created_at": "Fri Nov 16 00:21:34 +0000 2018",
 	"lang": "ja"
 }
--- data/js/tweets/2018_11.js
+++ data/js/tweets/2018_11.js
@@ -1,6 +1,344 @@
 Grailbird.data.tweets_2018_11 = 
 [
 	{
+		"created_at": "Wed Nov 14 15:58:10 +0000 2018",
+		"id": 1062736458052587500,
+		"id_str": "1062736458052587521",
+		"full_text": "남자 다섯 명이 여자 두 명을 “페미니스트 같다” 는 이유로 폭행해 두개골이 드러나는 중상을 입혔는데 한국 사회에 만연한 혐오범죄의 위험성이 아니라 여자가 “메갈” 인지 아닌지 “맞을 짓” 을 하지는 않았는지 여부를 문제 삼고 싶다면 인류의 유전자 풀에서 자신을 제거하는 것이 사회적 공헌이다.",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			164
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 315583544,
+			"id_str": "315583544",
+			"name": "피어스 호th론",
+			"screen_name": "sub_plot",
+			"location": "",
+			"description": "답은 헤러시다 은하계를 불태워라",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 12692,
+			"friends_count": 11032,
+			"listed_count": 157,
+			"created_at": "Sun Jun 12 03:34:43 +0000 2011",
+			"favourites_count": 746037,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 11651,
+			"lang": "ko",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "1A1B1F",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/543864389233692672/Iv92TTlQ_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/543864389233692672/Iv92TTlQ_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/315583544/1356864547",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "181A1E",
+			"profile_sidebar_fill_color": "252429",
+			"profile_text_color": "666666",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1570,
+		"favorite_count": 336,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "ko",
+		"text": "남자 다섯 명이 여자 두 명을 “페미니스트 같다” 는 이유로 폭행해 두개골이 드러나는 중상을 입혔는데 한국 사회에 만연한 혐오범죄의 위험성이 아니라 여자가 “메갈” 인지 아닌지 “맞을 짓” 을 하지는 않았는지 여부를 문제 삼고 싶다면 인류의 유전자 풀에서 자신을 제거하는 것이 사회적 공헌이다."
+	},
+	{
+		"created_at": "Wed Nov 14 00:21:43 +0000 2018",
+		"id": 1062500789283774500,
+		"id_str": "1062500789283774464",
+		"full_text": "なかなか斬新だな\n一瞬自分がすごい疲れて幻覚を見てるのかと思った https://t.co/mwUpKUKhSq",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			32
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1062500767804751900,
+					"id_str": "1062500767804751872",
+					"indices": [
+						33,
+						56
+					],
+					"media_url": "http://pbs.twimg.com/media/Dr7C0aaVAAAcGX5.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Dr7C0aaVAAAcGX5.jpg",
+					"url": "https://t.co/mwUpKUKhSq",
+					"display_url": "pic.twitter.com/mwUpKUKhSq",
+					"expanded_url": "https://twitter.com/yohar1114/status/1062500789283774464/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1024,
+							"h": 768,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1024,
+							"h": 768,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1062500767804751900,
+					"id_str": "1062500767804751872",
+					"indices": [
+						33,
+						56
+					],
+					"media_url": "http://pbs.twimg.com/media/Dr7C0aaVAAAcGX5.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Dr7C0aaVAAAcGX5.jpg",
+					"url": "https://t.co/mwUpKUKhSq",
+					"display_url": "pic.twitter.com/mwUpKUKhSq",
+					"expanded_url": "https://twitter.com/yohar1114/status/1062500789283774464/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1024,
+							"h": 768,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1024,
+							"h": 768,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 932808010296016900,
+			"id_str": "932808010296016896",
+			"name": "Yoshiyuki Harada",
+			"screen_name": "yohar1114",
+			"location": "",
+			"description": "Photographer Tokyo, Japan. ・星景、風景、たまーにポートレート。 ・東京都オフィシャルカメラマン/YourShot EF/etc. ・α7RIII/GM大三元/Otus55/RRS TVC-34L/Z1+/sdQuattro/etc \n・✉yohar1114@gmail.com",
+			"url": "https://t.co/ghUlWAkCjT",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/ghUlWAkCjT",
+							"expanded_url": "http://Instagram.com/yohar1114",
+							"display_url": "Instagram.com/yohar1114",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1272,
+			"friends_count": 42,
+			"listed_count": 11,
+			"created_at": "Tue Nov 21 03:08:55 +0000 2017",
+			"favourites_count": 300,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 238,
+			"lang": "ja",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/932808576795533312/-sNp2ps6_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/932808576795533312/-sNp2ps6_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/932808010296016896/1533125214",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 23872,
+		"favorite_count": 46470,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ja",
+		"text": "なかなか斬新だな\n一瞬自分がすごい疲れて幻覚を見てるのかと思った https://t.co/mwUpKUKhSq"
+	},
+	{
+		"created_at": "Wed Nov 14 01:00:43 +0000 2018",
+		"id": 1062510606324908000,
+		"id_str": "1062510606324908033",
+		"full_text": "예전 은평구 ‘두번째 부엌’ 기억하시는 분들. 지금은 동대문구청 옆에 있습니다.",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			44
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1062477664890445800,
+		"in_reply_to_status_id_str": "1062477664890445825",
+		"in_reply_to_user_id": 94077994,
+		"in_reply_to_user_id_str": "94077994",
+		"in_reply_to_screen_name": "kawanoboru",
+		"user": {
+			"id": 94077994,
+			"id_str": "94077994",
+			"name": "縄",
+			"screen_name": "kawanoboru",
+			"location": "마음 만은 언제나 오키나와",
+			"description": "沖縄戦後史を勉強してた、ソウルに住んでいる普通の会社員 오키나와 전후사를 공부한 적이 있는, 서울에 살고 있는 평범한 회사원",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 589,
+			"friends_count": 490,
+			"listed_count": 22,
+			"created_at": "Wed Dec 02 12:19:58 +0000 2009",
+			"favourites_count": 2060,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 16884,
+			"lang": "ja",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": true,
+			"profile_background_color": "B2DFDA",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme13/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme13/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/983561990789521409/yByV3Crc_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/983561990789521409/yByV3Crc_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/94077994/1441838938",
+			"profile_link_color": "93A644",
+			"profile_sidebar_border_color": "EEEEEE",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "ko",
+		"text": "예전 은평구 ‘두번째 부엌’ 기억하시는 분들. 지금은 동대문구청 옆에 있습니다."
+	},
+	{
 		"created_at": "Wed Nov 14 00:21:43 +0000 2018",
 		"id": 1062500789283774500,
 		"id_str": "1062500789283774464",
