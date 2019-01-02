1730d236734ca9ed438ae0609ff7f02522bc44a7
rosarux <seungwoo.ha@gmail.com>
2019-01-02 09:21:35 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 18012,
-	"created_at": "Tue Jan 01 00:21:35 +0000 2019",
+	"tweets": 18013,
+	"created_at": "Wed Jan 02 00:21:34 +0000 2019",
 	"lang": "ja"
 }
---
+++ data/js/tweets/2019_01.js
@@ -0,0 +1,98 @@
+Grailbird.data.tweets_2019_01 = 
+[
+	{
+		"created_at": "Mon Dec 31 15:21:53 +0000 2018",
+		"id": 1079759553426686000,
+		"id_str": "1079759553426685952",
+		"full_text": "먼저 새해인사 잘 보내지 않는 모든 내향인간들 새해복 많이 받으세요",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			37
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 99112948,
+			"id_str": "99112948",
+			"name": "ᴊᴇʀʀʏ.ᴋ",
+			"screen_name": "JerrykMusic",
+			"location": "Seoul",
+			"description": "JERRY.K | blent. | DAZE ALIVE",
+			"url": "https://t.co/mMoGb280jp",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/mMoGb280jp",
+							"expanded_url": "http://JerrykMusic.com",
+							"display_url": "JerrykMusic.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 36257,
+			"friends_count": 409,
+			"listed_count": 291,
+			"created_at": "Thu Dec 24 14:25:39 +0000 2009",
+			"favourites_count": 1781,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 69371,
+			"lang": "ko",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/966341204911648768/AxY7n9pv_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/966341204911648768/AxY7n9pv_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/99112948/1541754111",
+			"profile_link_color": "000000",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3328,
+		"favorite_count": 1809,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "ko",
+		"text": "먼저 새해인사 잘 보내지 않는 모든 내향인간들 새해복 많이 받으세요"
+	}
+]
