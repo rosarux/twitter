1d3ba3e33fdb44fe02cd038b2e1c137e1bd45e9e
rosarux <seungwoo.ha@gmail.com>
2019-01-03 09:21:35 +0900

    Added 3 tweets

--- data/js/tweets/2019_01.js
+++ data/js/tweets/2019_01.js
@@ -1,6 +1,409 @@
 Grailbird.data.tweets_2019_01 = 
 [
 	{
+		"created_at": "Wed Jan 02 05:35:00 +0000 2019",
+		"id": 1080336637576372200,
+		"id_str": "1080336637576372229",
+		"full_text": "그럼 귀족노조 가입하세요 와 개꿀 https://t.co/GkUSERiQCq",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			18
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/GkUSERiQCq",
+					"expanded_url": "https://twitter.com/insight9603/status/1080078046449754113",
+					"display_url": "twitter.com/insight9603/st…",
+					"indices": [
+						19,
+						42
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 91097138,
+			"id_str": "91097138",
+			"name": "빨덕후",
+			"screen_name": "Ryuzeh",
+			"location": "",
+			"description": "야 진짜 피곤하지 않냐 오늘? 대박이다 정말 profile pic by @lovely_aori",
+			"url": "https://t.co/9Vz1FYFtIu",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/9Vz1FYFtIu",
+							"expanded_url": "http://ryuzeh.wordpress.com/",
+							"display_url": "ryuzeh.wordpress.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3165,
+			"friends_count": 1428,
+			"listed_count": 129,
+			"created_at": "Thu Nov 19 12:43:12 +0000 2009",
+			"favourites_count": 2956,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 390294,
+			"lang": "ko",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "131516",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/848194226164822016/N3--UeCd_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/848194226164822016/N3--UeCd_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/91097138/1491060238",
+			"profile_link_color": "009999",
+			"profile_sidebar_border_color": "EEEEEE",
+			"profile_sidebar_fill_color": "EFEFEF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1080078046449754100,
+		"quoted_status_id_str": "1080078046449754113",
+		"quoted_status_permalink": {
+			"url": "https://t.co/GkUSERiQCq",
+			"expanded": "https://twitter.com/insight9603/status/1080078046449754113",
+			"display": "twitter.com/insight9603/st…"
+		},
+		"quoted_status": {
+			"created_at": "Tue Jan 01 12:27:27 +0000 2019",
+			"id": 1080078046449754100,
+			"id_str": "1080078046449754113",
+			"full_text": "@nyxity @ssamppam 노동자도, 노동자라고 다 같은 노동자가아닙니다.민노총같은 귀족노조 가입된 귀족노동자는 기본급보다 많은 수당을 받으며 연봉 7~8천만원받는가하면, 영세공장,자영업종사자는 10여년째 월급100여만원만 받으며 언제 짤릴지 모르는일을하죠. 귀족노조부터 분쇄해야 노동정책을 제대로 할수있습니다",
+			"truncated": false,
+			"display_text_range": [
+				18,
+				176
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "nyxity",
+						"name": "Jin",
+						"id": 21623729,
+						"id_str": "21623729",
+						"indices": [
+							0,
+							7
+						]
+					},
+					{
+						"screen_name": "ssamppam",
+						"name": "Ks Woo",
+						"id": 103548944,
+						"id_str": "103548944",
+						"indices": [
+							8,
+							17
+						]
+					}
+				],
+				"urls": []
+			},
+			"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+			"in_reply_to_status_id": 1079961470496960500,
+			"in_reply_to_status_id_str": "1079961470496960515",
+			"in_reply_to_user_id": 21623729,
+			"in_reply_to_user_id_str": "21623729",
+			"in_reply_to_screen_name": "nyxity",
+			"user": {
+				"id": 500456013,
+				"id_str": "500456013",
+				"name": "하얀바람",
+				"screen_name": "insight9603",
+				"location": "Buchoen, Seoul, Korea",
+				"description": "몸도 마음도 건강한 대한민국!\r\n정확한 정보와 합리적 판단으로 무엇이 진실이고, 무엇이 옳은것인가를 추구한다.\n나는 '상식'으로 판단한다!",
+				"url": "https://t.co/SfYW8c5cPB",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/SfYW8c5cPB",
+								"expanded_url": "http://twitaddons.com/insight9603",
+								"display_url": "twitaddons.com/insight9603",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 3062,
+				"friends_count": 3300,
+				"listed_count": 13,
+				"created_at": "Thu Feb 23 04:43:50 +0000 2012",
+				"favourites_count": 1406,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 6182,
+				"lang": "ko",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "C0DEED",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": true,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1847409235/_____1_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1847409235/_____1_normal.jpg",
+				"profile_link_color": "0084B4",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 10,
+			"favorite_count": 1,
+			"favorited": false,
+			"retweeted": false,
+			"lang": "ko"
+		},
+		"retweet_count": 123,
+		"favorite_count": 32,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "그럼 귀족노조 가입하세요 와 개꿀 https://t.co/GkUSERiQCq"
+	},
+	{
+		"created_at": "Wed Jan 02 07:54:25 +0000 2019",
+		"id": 1080371720366612500,
+		"id_str": "1080371720366612480",
+		"full_text": "술자리에서 기재부 사무관 후배의 고뇌에 찬 영웅담을 들으며 '그거 폭로하면 넌 스타가 된다'고 부추긴 고향선배나 학교선배가 아마 있었을 것 같다. 술 깨면 술자리 대화에서도 헤어나오는 지혜가 참 중요하다. 바보 되지 않으려면.",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			126
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 262913632,
+			"id_str": "262913632",
+			"name": "김은식",
+			"screen_name": "kimeunsik",
+			"location": "",
+			"description": "야구, 인물, 역사, 사회, 문화 등에 대해 많이 아는 사람은 아니고 많은 생각을 하는 사람입니다. 그리고 글을 씁니다.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 5446,
+			"friends_count": 3484,
+			"listed_count": 145,
+			"created_at": "Wed Mar 09 00:50:43 +0000 2011",
+			"favourites_count": 594,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 6712,
+			"lang": "ko",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "022330",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/635803756802609154/YfEymG-2_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/635803756802609154/YfEymG-2_normal.jpg",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 72,
+		"favorite_count": 57,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "ko",
+		"text": "술자리에서 기재부 사무관 후배의 고뇌에 찬 영웅담을 들으며 '그거 폭로하면 넌 스타가 된다'고 부추긴 고향선배나 학교선배가 아마 있었을 것 같다. 술 깨면 술자리 대화에서도 헤어나오는 지혜가 참 중요하다. 바보 되지 않으려면."
+	},
+	{
+		"created_at": "Mon Dec 31 15:21:53 +0000 2018",
+		"id": 1079759553426686000,
+		"id_str": "1079759553426685952",
+		"full_text": "먼저 새해인사 잘 보내지 않는 모든 내향인간들 새해복 많이 받으세요",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			37
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 99112948,
+			"id_str": "99112948",
+			"name": "ᴊᴇʀʀʏ.ᴋ",
+			"screen_name": "JerrykMusic",
+			"location": "Seoul",
+			"description": "JERRY.K | blent. | DAZE ALIVE",
+			"url": "https://t.co/mMoGb280jp",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/mMoGb280jp",
+							"expanded_url": "http://JerrykMusic.com",
+							"display_url": "JerrykMusic.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 36245,
+			"friends_count": 408,
+			"listed_count": 291,
+			"created_at": "Thu Dec 24 14:25:39 +0000 2009",
+			"favourites_count": 1781,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 69363,
+			"lang": "ko",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/966341204911648768/AxY7n9pv_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/966341204911648768/AxY7n9pv_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/99112948/1541754111",
+			"profile_link_color": "000000",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3930,
+		"favorite_count": 1992,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "ko",
+		"text": "먼저 새해인사 잘 보내지 않는 모든 내향인간들 새해복 많이 받으세요"
+	},
+	{
 		"created_at": "Mon Dec 31 15:21:53 +0000 2018",
 		"id": 1079759553426686000,
 		"id_str": "1079759553426685952",
