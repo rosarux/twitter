d8bb37f1aa3fb2a8aa0517c3c8a6997208a7c4f1
rosarux <seungwoo.ha@gmail.com>
2019-01-14 09:21:36 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 18058,
-	"created_at": "Sun Jan 13 00:21:34 +0000 2019",
+	"tweets": 18059,
+	"created_at": "Mon Jan 14 00:21:35 +0000 2019",
 	"lang": "ja"
 }
--- data/js/tweet_index.js
+++ data/js/tweet_index.js
@@ -3,7 +3,7 @@ var tweet_index = [
 		"file_name": "data/js/tweets/2019_01.js",
 		"year": 2018,
 		"var_name": "tweets_2019_01",
-		"tweet_count": 46,
+		"tweet_count": 47,
 		"month": 12
 	},
 	{
