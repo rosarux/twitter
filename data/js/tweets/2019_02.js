01dd91d4af34465fdbef2f21d0eb2b63453dbc86
rosarux <seungwoo.ha@gmail.com>
2019-02-03 09:21:36 +0900

    Added 3 tweets

--- data/js/tweets/2019_02.js
+++ data/js/tweets/2019_02.js
@@ -1,6 +1,348 @@
 Grailbird.data.tweets_2019_02 = 
 [
 	{
+		"created_at": "Sat Feb 02 14:33:24 +0000 2019",
+		"id": 1091706151366840300,
+		"id_str": "1091706151366840320",
+		"full_text": "앵유민 https://t.co/RRkIFsVWp3",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			3
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1091706142382673900,
+					"id_str": "1091706142382673920",
+					"indices": [
+						4,
+						27
+					],
+					"media_url": "http://pbs.twimg.com/media/DyaE88_U0AALgwl.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DyaE88_U0AALgwl.jpg",
+					"url": "https://t.co/RRkIFsVWp3",
+					"display_url": "pic.twitter.com/RRkIFsVWp3",
+					"expanded_url": "https://twitter.com/kawanoboru/status/1091706151366840320/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1348,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 448,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 790,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1091706142382673900,
+					"id_str": "1091706142382673920",
+					"indices": [
+						4,
+						27
+					],
+					"media_url": "http://pbs.twimg.com/media/DyaE88_U0AALgwl.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DyaE88_U0AALgwl.jpg",
+					"url": "https://t.co/RRkIFsVWp3",
+					"display_url": "pic.twitter.com/RRkIFsVWp3",
+					"expanded_url": "https://twitter.com/kawanoboru/status/1091706151366840320/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1348,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 448,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 790,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 94077994,
+			"id_str": "94077994",
+			"name": "縄",
+			"screen_name": "kawanoboru",
+			"location": "마음 만은 언제나 오키나와",
+			"description": "沖縄戦後史を勉強してた、ソウルに住んでいる普通の会社員 오키나와 전후사를 공부한 적이 있는, 서울에 살고 있는 평범한 회사원",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 574,
+			"friends_count": 489,
+			"listed_count": 22,
+			"created_at": "Wed Dec 02 12:19:58 +0000 2009",
+			"favourites_count": 2175,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 17059,
+			"lang": "ja",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": true,
+			"profile_background_color": "B2DFDA",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme13/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme13/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/983561990789521409/yByV3Crc_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/983561990789521409/yByV3Crc_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/94077994/1441838938",
+			"profile_link_color": "93A644",
+			"profile_sidebar_border_color": "EEEEEE",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "앵유민 https://t.co/RRkIFsVWp3"
+	},
+	{
+		"created_at": "Fri Feb 01 12:44:09 +0000 2019",
+		"id": 1091316273672183800,
+		"id_str": "1091554827853680640",
+		"full_text": "도지사의 위력에 일반인 남자가 왜 감정이입하냐면 다들 비루한 권력이라도 주어지면 적극적으로 위력 행사할 의지(범죄까지 안 가더라도)가 있고 그게 내 노력에 대한 정당한 보상이라고 생각하기 때문. 군대 보면 다 알잖누 RPG적 세계관과 가상적 자유의지.",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			140
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"http://twicca.r246.jp/\" rel=\"nofollow\">twicca</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 53655888,
+			"id_str": "53655888",
+			"name": "U보트 작전관 안빈락커(폐족)",
+			"screen_name": "anbinrocker",
+			"location": "시베리아 설원 근방 북극해",
+			"description": "가카의 마지막 충신",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 6037,
+			"friends_count": 380,
+			"listed_count": 162,
+			"created_at": "Sat Jul 04 12:07:41 +0000 2009",
+			"favourites_count": 3267,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 25559,
+			"lang": "ko",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1010171736409509889/d207BFMX_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1010171736409509889/d207BFMX_normal.jpg",
+			"profile_link_color": "25762E",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 69,
+		"favorite_count": 30,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "ko",
+		"text": "도지사의 위력에 일반인 남자가 왜 감정이입하냐면 다들 비루한 권력이라도 주어지면 적극적으로 위력 행사할 의지(범죄까지 안 가더라도)가 있고 그게 내 노력에 대한 정당한 보상이라고 생각하기 때문. 군대 보면 다 알잖누 RPG적 세계관과 가상적 자유의지."
+	},
+	{
+		"created_at": "Fri Feb 01 19:21:26 +0000 2019",
+		"id": 1091416251044372500,
+		"id_str": "1091553099230003200",
+		"full_text": "이렇게 중요한 문제가 결론이 나다니.. 노벨상 줘라 https://t.co/8k3GrVJBNw",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			28
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/8k3GrVJBNw",
+					"expanded_url": "https://twitter.com/yonhaptweet/status/1091415931862085633",
+					"display_url": "twitter.com/yonhaptweet/st…",
+					"indices": [
+						29,
+						52
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 48618681,
+			"id_str": "48618681",
+			"name": "the 누",
+			"screen_name": "k_ch_cucumber",
+			"location": "New York, USA",
+			"description": "더민주의 the가 아니지만.. 아무튼 음악 음식 연구 그 외 잡소리 합니다.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 620,
+			"friends_count": 460,
+			"listed_count": 13,
+			"created_at": "Fri Jun 19 05:15:58 +0000 2009",
+			"favourites_count": 5811,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 261,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/573634208863817728/n3C4TXlV_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/573634208863817728/n3C4TXlV_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/48618681/1520777428",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1091415931862085600,
+		"quoted_status_id_str": "1091415931862085633",
+		"quoted_status_permalink": {
+			"url": "https://t.co/8k3GrVJBNw",
+			"expanded": "https://twitter.com/yonhaptweet/status/1091415931862085633",
+			"display": "twitter.com/yonhaptweet/st…"
+		},
+		"retweet_count": 376,
+		"favorite_count": 83,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "이렇게 중요한 문제가 결론이 나다니.. 노벨상 줘라 https://t.co/8k3GrVJBNw"
+	},
+	{
 		"created_at": "Fri Feb 01 05:35:05 +0000 2019",
 		"id": 1091208292016087000,
 		"id_str": "1091208941185294336",
