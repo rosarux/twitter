37875ce0030f5da5fe517f91e1ebd2d934d967cb
rosarux <seungwoo.ha@gmail.com>
2019-03-21 09:21:35 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 18196,
-	"created_at": "Wed Mar 20 00:21:34 +0000 2019",
+	"tweets": 18197,
+	"created_at": "Thu Mar 21 00:21:35 +0000 2019",
 	"lang": "ja"
 }
--- data/js/tweets/2019_03.js
+++ data/js/tweets/2019_03.js
@@ -1,6 +1,210 @@
 Grailbird.data.tweets_2019_03 = 
 [
 	{
+		"created_at": "Wed Mar 20 08:36:53 +0000 2019",
+		"id": 1108156931368087600,
+		"id_str": "1108286274278817792",
+		"full_text": "天然記念物で絶滅危惧種のジュゴンが死んでしまいました。先日、鳥羽の水族館で食欲旺盛なジュゴンを見たばかりでした。その時、辺野古には餌がなくなっているのではないかと心配したのです。辺野古の埋め立てで遂に死んでしまいました。人間のエゴで生き物を殺すのは止めにしようではありませんか https://t.co/PDEKj2OI1Y",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			138
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1108156875420258300,
+					"id_str": "1108156875420258304",
+					"indices": [
+						139,
+						162
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1108156875420258304/pu/img/UOVZpy8OnoAQZeDc.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1108156875420258304/pu/img/UOVZpy8OnoAQZeDc.jpg",
+					"url": "https://t.co/PDEKj2OI1Y",
+					"display_url": "pic.twitter.com/PDEKj2OI1Y",
+					"expanded_url": "https://twitter.com/hatoyamayukio/status/1108156931368087555/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 960,
+							"h": 540,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 960,
+							"h": 540,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1108156875420258300,
+					"id_str": "1108156875420258304",
+					"indices": [
+						139,
+						162
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1108156875420258304/pu/img/UOVZpy8OnoAQZeDc.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1108156875420258304/pu/img/UOVZpy8OnoAQZeDc.jpg",
+					"url": "https://t.co/PDEKj2OI1Y",
+					"display_url": "pic.twitter.com/PDEKj2OI1Y",
+					"expanded_url": "https://twitter.com/hatoyamayukio/status/1108156931368087555/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 960,
+							"h": 540,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 960,
+							"h": 540,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							16,
+							9
+						],
+						"duration_millis": 17365,
+						"variants": [
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1108156875420258304/pu/vid/640x360/ESM7j8wtGRttuxYZ.mp4?tag=8"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1108156875420258304/pu/pl/UKdLwPD6R6A2WF_6.m3u8?tag=8"
+							},
+							{
+								"bitrate": 256000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1108156875420258304/pu/vid/320x180/ehvx9nox8Tti-ypJ.mp4?tag=8"
+							},
+							{
+								"bitrate": 2176000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1108156875420258304/pu/vid/960x540/PzhcJ7wQubMYNp4S.mp4?tag=8"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 97244324,
+			"id_str": "97244324",
+			"name": "鳩山由紀夫",
+			"screen_name": "hatoyamayukio",
+			"location": "JAPAN",
+			"description": "政治の話に限らず、身辺に起こったいろいろな話題について書いていければと思っています。ご意見ご感想は @hatoyamayukio でお願いいたします。",
+			"url": "http://t.co/7UM0Rv8toT",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/7UM0Rv8toT",
+							"expanded_url": "http://www.eaci.or.jp/",
+							"display_url": "eaci.or.jp",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 829649,
+			"friends_count": 117897,
+			"listed_count": 26382,
+			"created_at": "Wed Dec 16 16:44:30 +0000 2009",
+			"favourites_count": 10,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 1011,
+			"lang": "ja",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "5BB007",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/603040601/hatoyama_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/603040601/hatoyama_normal.png",
+			"profile_link_color": "0DA100",
+			"profile_sidebar_border_color": "DBDBDB",
+			"profile_sidebar_fill_color": "F2F2F2",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3523,
+		"favorite_count": 5537,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ja",
+		"text": "天然記念物で絶滅危惧種のジュゴンが死んでしまいました。先日、鳥羽の水族館で食欲旺盛なジュゴンを見たばかりでした。その時、辺野古には餌がなくなっているのではないかと心配したのです。辺野古の埋め立てで遂に死んでしまいました。人間のエゴで生き物を殺すのは止めにしようではありませんか https://t.co/PDEKj2OI1Y"
+	},
+	{
 		"created_at": "Tue Mar 19 13:52:59 +0000 2019",
 		"id": 1108001933388927000,
 		"id_str": "1108003434211340289",
