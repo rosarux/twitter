71e5a7f4063a4cefc32399ebfc06d3c729f56e5e
rosarux <seungwoo.ha@gmail.com>
2019-03-31 09:21:35 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 18210,
-	"created_at": "Sat Mar 30 00:21:36 +0000 2019",
+	"tweets": 18211,
+	"created_at": "Sun Mar 31 00:21:35 +0000 2019",
 	"lang": "ja"
 }
--- data/js/tweets/2019_03.js
+++ data/js/tweets/2019_03.js
@@ -1,6 +1,167 @@
 Grailbird.data.tweets_2019_03 = 
 [
 	{
+		"created_at": "Sat Mar 30 14:53:13 +0000 2019",
+		"id": 1111469360843898900,
+		"id_str": "1112004859404775424",
+		"full_text": "綺麗だろ みんな残業だぜ これ https://t.co/HPrvsNPFRV",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			15
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1111469353969410000,
+					"id_str": "1111469353969410048",
+					"indices": [
+						16,
+						39
+					],
+					"media_url": "http://pbs.twimg.com/media/D2y7fRYUYAAM1E8.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D2y7fRYUYAAM1E8.jpg",
+					"url": "https://t.co/HPrvsNPFRV",
+					"display_url": "pic.twitter.com/HPrvsNPFRV",
+					"expanded_url": "https://twitter.com/Dylan_ek318c/status/1111469360843898881/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1024,
+							"h": 681,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 452,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1024,
+							"h": 681,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1111469353969410000,
+					"id_str": "1111469353969410048",
+					"indices": [
+						16,
+						39
+					],
+					"media_url": "http://pbs.twimg.com/media/D2y7fRYUYAAM1E8.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D2y7fRYUYAAM1E8.jpg",
+					"url": "https://t.co/HPrvsNPFRV",
+					"display_url": "pic.twitter.com/HPrvsNPFRV",
+					"expanded_url": "https://twitter.com/Dylan_ek318c/status/1111469360843898881/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1024,
+							"h": 681,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 452,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1024,
+							"h": 681,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1030036933,
+			"id_str": "1030036933",
+			"name": "でぃらん",
+			"screen_name": "Dylan_ek318c",
+			"location": "Hiroshima Japan",
+			"description": "シビックに乗ってます。目指せスポコン\nよろしくお願いします。\n#EK3\n#B18C\n#ウェーバースポーツ",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 790,
+			"friends_count": 937,
+			"listed_count": 5,
+			"created_at": "Sun Dec 23 06:54:56 +0000 2012",
+			"favourites_count": 5514,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 4405,
+			"lang": "ja",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1111021034638139392/FcFaD6zD_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1111021034638139392/FcFaD6zD_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1030036933/1551567503",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 21441,
+		"favorite_count": 53689,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ja",
+		"text": "綺麗だろ みんな残業だぜ これ https://t.co/HPrvsNPFRV"
+	},
+	{
 		"created_at": "Fri Mar 29 14:09:22 +0000 2019",
 		"id": 1111620078670221300,
 		"id_str": "1111631438615666689",
