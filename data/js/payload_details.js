7b1cf7e41ddaf03a0e765157c80102842451d1d4
rosarux <seungwoo.ha@gmail.com>
2019-04-04 09:21:35 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 18214,
-	"created_at": "Wed Apr 03 00:21:34 +0000 2019",
+	"tweets": 18215,
+	"created_at": "Thu Apr 04 00:21:35 +0000 2019",
 	"lang": "ja"
 }
--- data/js/tweets/2019_04.js
+++ data/js/tweets/2019_04.js
@@ -1,6 +1,275 @@
 Grailbird.data.tweets_2019_04 = 
 [
 	{
+		"created_at": "Wed Apr 03 09:43:28 +0000 2019",
+		"id": 1113352170240303100,
+		"id_str": "1113376460637474816",
+		"full_text": "이번 연호 발표 합성짤 중 페이보릿 4개 https://t.co/L8dK6odnFp",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			22
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1113352163822960600,
+					"id_str": "1113352163822960641",
+					"indices": [
+						23,
+						46
+					],
+					"media_url": "http://pbs.twimg.com/media/D3Nr5OqUgAEb8ZD.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D3Nr5OqUgAEb8ZD.jpg",
+					"url": "https://t.co/L8dK6odnFp",
+					"display_url": "pic.twitter.com/L8dK6odnFp",
+					"expanded_url": "https://twitter.com/xywaz/status/1113352170240303104/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 473,
+							"h": 640,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 473,
+							"h": 640,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 473,
+							"h": 640,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1113352163822960600,
+					"id_str": "1113352163822960641",
+					"indices": [
+						23,
+						46
+					],
+					"media_url": "http://pbs.twimg.com/media/D3Nr5OqUgAEb8ZD.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D3Nr5OqUgAEb8ZD.jpg",
+					"url": "https://t.co/L8dK6odnFp",
+					"display_url": "pic.twitter.com/L8dK6odnFp",
+					"expanded_url": "https://twitter.com/xywaz/status/1113352170240303104/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 473,
+							"h": 640,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 473,
+							"h": 640,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 473,
+							"h": 640,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1113352163818856400,
+					"id_str": "1113352163818856449",
+					"indices": [
+						23,
+						46
+					],
+					"media_url": "http://pbs.twimg.com/media/D3Nr5OpV4AE4f82.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D3Nr5OpV4AE4f82.jpg",
+					"url": "https://t.co/L8dK6odnFp",
+					"display_url": "pic.twitter.com/L8dK6odnFp",
+					"expanded_url": "https://twitter.com/xywaz/status/1113352170240303104/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 470,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1200,
+							"h": 829,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 829,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1113352163823050800,
+					"id_str": "1113352163823050752",
+					"indices": [
+						23,
+						46
+					],
+					"media_url": "http://pbs.twimg.com/media/D3Nr5OqV4AALEsG.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D3Nr5OqV4AALEsG.jpg",
+					"url": "https://t.co/L8dK6odnFp",
+					"display_url": "pic.twitter.com/L8dK6odnFp",
+					"expanded_url": "https://twitter.com/xywaz/status/1113352170240303104/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1113352163814568000,
+					"id_str": "1113352163814567946",
+					"indices": [
+						23,
+						46
+					],
+					"media_url": "http://pbs.twimg.com/media/D3Nr5OoUcAoA6Ep.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D3Nr5OoUcAoA6Ep.jpg",
+					"url": "https://t.co/L8dK6odnFp",
+					"display_url": "pic.twitter.com/L8dK6odnFp",
+					"expanded_url": "https://twitter.com/xywaz/status/1113352170240303104/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1200,
+							"h": 808,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1200,
+							"h": 808,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 458,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 895439053612609500,
+			"id_str": "895439053612609536",
+			"name": "휘 밧",
+			"screen_name": "xywaz",
+			"location": "",
+			"description": "돈 주면 동의RT 합니다",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2958,
+			"friends_count": 321,
+			"listed_count": 19,
+			"created_at": "Thu Aug 10 00:18:01 +0000 2017",
+			"favourites_count": 58491,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 53029,
+			"lang": "ko",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1058338822100987904/ALvBsTTq_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1058338822100987904/ALvBsTTq_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/895439053612609536/1502330373",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1776,
+		"favorite_count": 537,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "이번 연호 발표 합성짤 중 페이보릿 4개 https://t.co/L8dK6odnFp"
+	},
+	{
 		"created_at": "Tue Apr 02 12:17:59 +0000 2019",
 		"id": 1113051140436910100,
 		"id_str": "1113052957635833856",
