c4f04ccc816cfb6264ca99ec19eeb26c86806d35
rosarux <seungwoo.ha@gmail.com>
2019-05-02 09:21:36 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 18267,
-	"created_at": "Wed May 01 00:21:35 +0000 2019",
+	"tweets": 18268,
+	"created_at": "Thu May 02 00:21:35 +0000 2019",
 	"lang": "ja"
 }
---
+++ data/js/tweets/2019_05.js
@@ -0,0 +1,85 @@
+Grailbird.data.tweets_2019_05 = 
+[
+	{
+		"created_at": "Wed May 01 06:55:32 +0000 2019",
+		"id": 1123430066896109600,
+		"id_str": "1123481061240004608",
+		"full_text": "한국 언론들 너무 무능하다. 헤이세이 천황이 생전에 왕위를 넘겨서 일본 국회 마비시키고 집권 내에 일본 군사력을 키우려 했던 아베를 정치적으로 어떻게 엿먹였는지 이게 한국에 어떻게 영향을 끼치는지 분석해도 모자랄 판에 여돌 한 명이 연호가 바뀌었네요 하는 인스타나 물고 늘어지고 있음.",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			159
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2932172028,
+			"id_str": "2932172028",
+			"name": "도사",
+			"screen_name": "healthjeondosa",
+			"location": " ",
+			"description": "*비팔알림X. 디엠주세요* 성씨없애기 운동본부. 생활동반자법, 차별금지법! #성씨없애기운동 #여성작가_여성서사_알티이벤트\n#여성서사_추천타래 #여혐기사댓글 #불매리스트",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 4408,
+			"friends_count": 324,
+			"listed_count": 42,
+			"created_at": "Tue Dec 16 12:33:39 +0000 2014",
+			"favourites_count": 19107,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 108416,
+			"lang": "ko",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1106579072899940354/t20SP6ck_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1106579072899940354/t20SP6ck_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2932172028/1530482399",
+			"profile_link_color": "981CEB",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 14310,
+		"favorite_count": 3399,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "ko",
+		"text": "한국 언론들 너무 무능하다. 헤이세이 천황이 생전에 왕위를 넘겨서 일본 국회 마비시키고 집권 내에 일본 군사력을 키우려 했던 아베를 정치적으로 어떻게 엿먹였는지 이게 한국에 어떻게 영향을 끼치는지 분석해도 모자랄 판에 여돌 한 명이 연호가 바뀌었네요 하는 인스타나 물고 늘어지고 있음."
+	}
+]
