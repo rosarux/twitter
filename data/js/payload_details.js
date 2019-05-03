a8ec0354505421d1072b8ed27d70464fa84168fe
rosarux <seungwoo.ha@gmail.com>
2019-05-03 09:21:35 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 18268,
-	"created_at": "Thu May 02 00:21:35 +0000 2019",
+	"tweets": 18269,
+	"created_at": "Fri May 03 00:21:34 +0000 2019",
 	"lang": "ja"
 }
--- data/js/tweets/2019_05.js
+++ data/js/tweets/2019_05.js
@@ -1,6 +1,188 @@
 Grailbird.data.tweets_2019_05 = 
 [
 	{
+		"created_at": "Thu May 02 06:34:17 +0000 2019",
+		"id": 1123837709548757000,
+		"id_str": "1123838099300200448",
+		"full_text": "#糸数慶子　さん描いてみましたー。髪がなびく感じと琉装ということで、髪を束ねる前、琉装の途中っていう事にしました。糸数慶子さんにはまだまだ活躍して欲しいですね。 https://t.co/88zsGRwQWm",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			80
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "糸数慶子",
+					"indices": [
+						0,
+						5
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1123837637738008600,
+					"id_str": "1123837637738008576",
+					"indices": [
+						81,
+						104
+					],
+					"media_url": "http://pbs.twimg.com/media/D5isYI6WkAAZ9iG.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D5isYI6WkAAZ9iG.jpg",
+					"url": "https://t.co/88zsGRwQWm",
+					"display_url": "pic.twitter.com/88zsGRwQWm",
+					"expanded_url": "https://twitter.com/TOYOsanshin/status/1123837709548756993/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 412,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1457,
+							"h": 883,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 727,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1123837637738008600,
+					"id_str": "1123837637738008576",
+					"indices": [
+						81,
+						104
+					],
+					"media_url": "http://pbs.twimg.com/media/D5isYI6WkAAZ9iG.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D5isYI6WkAAZ9iG.jpg",
+					"url": "https://t.co/88zsGRwQWm",
+					"display_url": "pic.twitter.com/88zsGRwQWm",
+					"expanded_url": "https://twitter.com/TOYOsanshin/status/1123837709548756993/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 412,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1457,
+							"h": 883,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 727,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 280534792,
+			"id_str": "280534792",
+			"name": "TOYO",
+			"screen_name": "TOYOsanshin",
+			"location": "",
+			"description": "okinawan sanshin player　沖縄への祈りを込めて。ご先祖様の苦難と平和の想いを受け継ぎ未来へ。たとえ自分が無力だとしても平和の礎の中の一つの石ころでありたい。宮古島生まれ首里育ち東京在住三線ミュージシャンでアーティスト。  ↓翁長知事追悼、StopHenoko琉球人魚　玉城デニー応援",
+			"url": "https://t.co/3qG8cNRfZQ",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/3qG8cNRfZQ",
+							"expanded_url": "https://www.youtube.com/user/toyobox",
+							"display_url": "youtube.com/user/toyobox",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2016,
+			"friends_count": 1067,
+			"listed_count": 23,
+			"created_at": "Mon Apr 11 14:32:13 +0000 2011",
+			"favourites_count": 7941,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 5314,
+			"lang": "ja",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/824813581677891585/PQq68C5u_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/824813581677891585/PQq68C5u_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/280534792/1497117665",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 48,
+		"favorite_count": 119,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ja",
+		"text": "#糸数慶子　さん描いてみましたー。髪がなびく感じと琉装ということで、髪を束ねる前、琉装の途中っていう事にしました。糸数慶子さんにはまだまだ活躍して欲しいですね。 https://t.co/88zsGRwQWm"
+	},
+	{
 		"created_at": "Wed May 01 06:55:32 +0000 2019",
 		"id": 1123430066896109600,
 		"id_str": "1123481061240004608",
