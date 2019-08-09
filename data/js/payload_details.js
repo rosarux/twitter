420f0317f317e77caba12e304e31ea4df39709ab
rosarux <seungwoo.ha@gmail.com>
2019-08-09 09:21:36 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 18653,
-	"created_at": "Thu Aug 08 00:21:35 +0000 2019",
+	"tweets": 18654,
+	"created_at": "Fri Aug 09 00:21:35 +0000 2019",
 	"lang": "ja"
 }
--- data/js/tweets/2019_08.js
+++ data/js/tweets/2019_08.js
@@ -1,6 +1,219 @@
 Grailbird.data.tweets_2019_08 = 
 [
 	{
+		"created_at": "Thu Aug 08 12:43:09 +0000 2019",
+		"id": 1159413393167540200,
+		"id_str": "1159444936925704193",
+		"full_text": "総がかり行動実行委員会から菱山南帆子さんです。\n\n「安倍首相は韓国に対する不当な経済報復を即刻撤回せよ！官邸前行動」\n\n8/27 総がかりと3・1実行委員会共催で18:30よりホワイト国除外施行日前日に、官邸前で抗議行動を行います！\n\n#東北アジア市民連帯行動 https://t.co/N08NhEwouu",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			130
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "東北アジア市民連帯行動",
+					"indices": [
+						118,
+						130
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1159413220261552000,
+					"id_str": "1159413220261552128",
+					"indices": [
+						131,
+						154
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1159413220261552128/pu/img/JP-oYmsaxKpSxM6_.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1159413220261552128/pu/img/JP-oYmsaxKpSxM6_.jpg",
+					"url": "https://t.co/N08NhEwouu",
+					"display_url": "pic.twitter.com/N08NhEwouu",
+					"expanded_url": "https://twitter.com/48daisyukai/status/1159413393167540224/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 383,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 720,
+							"h": 1280,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 675,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1159413220261552000,
+					"id_str": "1159413220261552128",
+					"indices": [
+						131,
+						154
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1159413220261552128/pu/img/JP-oYmsaxKpSxM6_.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1159413220261552128/pu/img/JP-oYmsaxKpSxM6_.jpg",
+					"url": "https://t.co/N08NhEwouu",
+					"display_url": "pic.twitter.com/N08NhEwouu",
+					"expanded_url": "https://twitter.com/48daisyukai/status/1159413393167540224/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 383,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 720,
+							"h": 1280,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 675,
+							"h": 1200,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							9,
+							16
+						],
+						"duration_millis": 24600,
+						"variants": [
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1159413220261552128/pu/pl/FOONzsB5EbCqZUvM.m3u8?tag=10"
+							},
+							{
+								"bitrate": 2176000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1159413220261552128/pu/vid/720x1280/HuQp4VC9b2TadvJQ.mp4?tag=10"
+							},
+							{
+								"bitrate": 632000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1159413220261552128/pu/vid/320x568/Vcjipz-QBGWd-Yuw.mp4?tag=10"
+							},
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1159413220261552128/pu/vid/360x640/ZUc05R2zmT3hR7sX.mp4?tag=10"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2412148272,
+			"id_str": "2412148272",
+			"name": "憲法９条を壊すな！実行委員会",
+			"screen_name": "48daisyukai",
+			"location": "",
+			"description": "平和をねがい、戦争に反対するすべてのみなさん。\nいまこそ、ともに声をあげましょう。行動を起こしましょう。",
+			"url": "https://t.co/Od7fvQpZvG",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Od7fvQpZvG",
+							"expanded_url": "http://kyujokowasuna.com/",
+							"display_url": "kyujokowasuna.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 13886,
+			"friends_count": 1882,
+			"listed_count": 301,
+			"created_at": "Wed Mar 26 06:19:30 +0000 2014",
+			"favourites_count": 1246,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 31309,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/473513457837293568/z3zzUM4F_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/473513457837293568/z3zzUM4F_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2412148272/1403587467",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 306,
+		"favorite_count": 352,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ja",
+		"text": "総がかり行動実行委員会から菱山南帆子さんです。\n\n「安倍首相は韓国に対する不当な経済報復を即刻撤回せよ！官邸前行動」\n\n8/27 総がかりと3・1実行委員会共催で18:30よりホワイト国除外施行日前日に、官邸前で抗議行動を行います！\n\n#東北アジア市民連帯行動 https://t.co/N08NhEwouu"
+	},
+	{
 		"created_at": "Wed Aug 07 23:55:23 +0000 2019",
 		"id": 1159247231846760400,
 		"id_str": "1159251723443032064",
