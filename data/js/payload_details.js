94ff41f1db4c246c3f3422f2d78b9d4f0235434e
rosarux <seungwoo.ha@gmail.com>
2019-09-02 09:21:35 +0900

    Added 3 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 18681,
-	"created_at": "Tue Aug 27 00:21:36 +0000 2019",
+	"tweets": 18684,
+	"created_at": "Mon Sep 02 00:21:35 +0000 2019",
 	"lang": "ja"
 }
---
+++ data/js/tweets/2019_09.js
@@ -0,0 +1,523 @@
+Grailbird.data.tweets_2019_09 = 
+[
+	{
+		"created_at": "Sun Sep 01 23:51:07 +0000 2019",
+		"id": 1168310346789965800,
+		"id_str": "1168310346789965824",
+		"full_text": "요즘 시국이 시국이니만큼 오키나와 복귀를 둘러싸고도 똥멍충이 같은 말들이 여기저기서 튀어나오는 구나",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			55
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 94077994,
+			"id_str": "94077994",
+			"name": "縄",
+			"screen_name": "kawanoboru",
+			"location": "마음 만은 언제나 오키나와",
+			"description": "沖縄戦後史を勉強してた、ソウルに住んでいる普通の会社員 오키나와 전후사를 공부한 적이 있는, 서울에 살고 있는 평범한 회사원",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 578,
+			"friends_count": 500,
+			"listed_count": 21,
+			"created_at": "Wed Dec 02 12:19:58 +0000 2009",
+			"favourites_count": 2490,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 17689,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": true,
+			"profile_background_color": "B2DFDA",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme13/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme13/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/983561990789521409/yByV3Crc_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/983561990789521409/yByV3Crc_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/94077994/1441838938",
+			"profile_link_color": "93A644",
+			"profile_sidebar_border_color": "EEEEEE",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "ko",
+		"text": "요즘 시국이 시국이니만큼 오키나와 복귀를 둘러싸고도 똥멍충이 같은 말들이 여기저기서 튀어나오는 구나"
+	},
+	{
+		"created_at": "Sun Sep 01 13:10:41 +0000 2019",
+		"id": 1168028828775538700,
+		"id_str": "1168149175403339776",
+		"full_text": "RIP Immanuel Wallerstein, one of the great intellectuals of our era, a social theorist who lived a deep commitment to justice, a warm human being who kept hope alive despite gloomy odds, and a good friend.",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			205
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 64001425,
+			"id_str": "64001425",
+			"name": "Craig Calhoun",
+			"screen_name": "craigjcalhoun",
+			"location": "Paradise Valley, AZ",
+			"description": "University Professor of Social Sciences at ASU. Centennial Professor at the LSE.",
+			"url": "https://t.co/l6BzBXnBmv",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/l6BzBXnBmv",
+							"expanded_url": "https://spgs.asu.edu/content/craig-calhoun",
+							"display_url": "spgs.asu.edu/content/craig-…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 21042,
+			"friends_count": 3012,
+			"listed_count": 619,
+			"created_at": "Sat Aug 08 17:09:07 +0000 2009",
+			"favourites_count": 8556,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 15077,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/768799866252324864/VlFUeHXw_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/768799866252324864/VlFUeHXw_normal.jpg",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": {
+			"id": "0019e2618c34b6b2",
+			"url": "https://api.twitter.com/1.1/geo/id/0019e2618c34b6b2.json",
+			"place_type": "city",
+			"name": "Goleta",
+			"full_name": "Goleta, CA",
+			"country_code": "US",
+			"country": "United States",
+			"contained_within": [],
+			"bounding_box": {
+				"type": "Polygon",
+				"coordinates": [
+					[
+						[
+							-119.9132679,
+							34.40581
+						],
+						[
+							-119.744123,
+							34.40581
+						],
+						[
+							-119.744123,
+							34.473116
+						],
+						[
+							-119.9132679,
+							34.473116
+						]
+					]
+				]
+			},
+			"attributes": {}
+		},
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1098,
+		"favorite_count": 2338,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "en",
+		"text": "RIP Immanuel Wallerstein, one of the great intellectuals of our era, a social theorist who lived a deep commitment to justice, a warm human being who kept hope alive despite gloomy odds, and a good friend."
+	},
+	{
+		"created_at": "Sun Sep 01 13:09:47 +0000 2019",
+		"id": 1167875854543679500,
+		"id_str": "1168148950097858561",
+		"full_text": "난 홍콩사람 인데요.우리는 여러분들이 홍콩의 정황을 알아줬으면 합니다.홍콩뉴스를 친구에게 가족에게 공유해주면 좋겠슴다. 진짜 감사합니다. 한국말을 못해서 너무 죄송합니다.\n#홍콩경찰 #홍콩 #한국 #민주주의 https://t.co/1Y7cmQGB5l",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			115
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "홍콩경찰",
+					"indices": [
+						96,
+						101
+					]
+				},
+				{
+					"text": "홍콩",
+					"indices": [
+						102,
+						105
+					]
+				},
+				{
+					"text": "한국",
+					"indices": [
+						106,
+						109
+					]
+				},
+				{
+					"text": "민주주의",
+					"indices": [
+						110,
+						115
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/1Y7cmQGB5l",
+					"expanded_url": "https://twitter.com/krstandwithhk/status/1167828670133571584",
+					"display_url": "twitter.com/krstandwithhk/…",
+					"indices": [
+						116,
+						139
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1167870446001279000,
+			"id_str": "1167870446001278979",
+			"name": "홍콩을 도와주세요",
+			"screen_name": "PlzHelpHKers",
+			"location": "",
+			"description": "홍콩사람인데요 잘부탁드려용 중국어/영어 가능해요 한국어 조금 할줄압니다",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 200,
+			"friends_count": 2,
+			"listed_count": 1,
+			"created_at": "Sat Aug 31 18:43:13 +0000 2019",
+			"favourites_count": 128,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 68,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1168240281021366272/KTFIQ0x6_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1168240281021366272/KTFIQ0x6_normal.jpg",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1167828670133571600,
+		"quoted_status_id_str": "1167828670133571584",
+		"quoted_status_permalink": {
+			"url": "https://t.co/1Y7cmQGB5l",
+			"expanded": "https://twitter.com/krstandwithhk/status/1167828670133571584",
+			"display": "twitter.com/krstandwithhk/…"
+		},
+		"quoted_status": {
+			"created_at": "Sat Aug 31 15:57:06 +0000 2019",
+			"id": 1167828670133571600,
+			"id_str": "1167828670133571584",
+			"full_text": "[홍콩경찰들이 시위자한테 진짜 총알로 쐈다]\n\n오늘 밤 시위중, 어떤 시민이 빅토리아파크에서 한 9mm 탄핀을 발견 했습니다. 이 후, 경찰 측에서 어떤 경찰은 진짜 총을 두 발을 쐈다는 게 인정 했다.\n\n자료출처: 입장신문 Stand News/ FactWire https://t.co/CG1RUuOVE8",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				146
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 1167828659450699800,
+						"id_str": "1167828659450699776",
+						"indices": [
+							147,
+							170
+						],
+						"media_url": "http://pbs.twimg.com/media/EDT1-nUUYAA7Wd_.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/EDT1-nUUYAA7Wd_.jpg",
+						"url": "https://t.co/CG1RUuOVE8",
+						"display_url": "pic.twitter.com/CG1RUuOVE8",
+						"expanded_url": "https://twitter.com/krstandwithhk/status/1167828670133571584/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 831,
+								"h": 900,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 831,
+								"h": 900,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 628,
+								"h": 680,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1167828659450699800,
+						"id_str": "1167828659450699776",
+						"indices": [
+							147,
+							170
+						],
+						"media_url": "http://pbs.twimg.com/media/EDT1-nUUYAA7Wd_.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/EDT1-nUUYAA7Wd_.jpg",
+						"url": "https://t.co/CG1RUuOVE8",
+						"display_url": "pic.twitter.com/CG1RUuOVE8",
+						"expanded_url": "https://twitter.com/krstandwithhk/status/1167828670133571584/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 831,
+								"h": 900,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 831,
+								"h": 900,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 628,
+								"h": 680,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 1162374234309611500,
+				"id_str": "1162374234309611523",
+				"name": "홍콩 인도법 시위 지지해주세요",
+				"screen_name": "krstandwithhk",
+				"location": "Hong Kong",
+				"description": "홍콩 인도법 시위 응원 해주신 분 너무 감사합니다. 큰 힘이 되주었습니다.  생방송:  https://t.co/q86rMEYK0Q",
+				"url": "https://t.co/MVdlxnjIOq",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/MVdlxnjIOq",
+								"expanded_url": "https://www.instagram.com/krstandwithhk/",
+								"display_url": "instagram.com/krstandwithhk/",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": [
+							{
+								"url": "https://t.co/q86rMEYK0Q",
+								"expanded_url": "https://ncehk2019.github.io/nce-live",
+								"display_url": "ncehk2019.github.io/nce-live",
+								"indices": [
+									49,
+									72
+								]
+							}
+						]
+					}
+				},
+				"protected": false,
+				"followers_count": 1788,
+				"friends_count": 0,
+				"listed_count": 1,
+				"created_at": "Fri Aug 16 14:43:14 +0000 2019",
+				"favourites_count": 10,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 18,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "F5F8FA",
+				"profile_background_image_url": null,
+				"profile_background_image_url_https": null,
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1162606918369775616/A3XzlTim_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1162606918369775616/A3XzlTim_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/1162374234309611523/1567351239",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 7313,
+			"favorite_count": 1222,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "ko"
+		},
+		"retweet_count": 17879,
+		"favorite_count": 3091,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "난 홍콩사람 인데요.우리는 여러분들이 홍콩의 정황을 알아줬으면 합니다.홍콩뉴스를 친구에게 가족에게 공유해주면 좋겠슴다. 진짜 감사합니다. 한국말을 못해서 너무 죄송합니다.\n#홍콩경찰 #홍콩 #한국 #민주주의 https://t.co/1Y7cmQGB5l"
+	}
+]
