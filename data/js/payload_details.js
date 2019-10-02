ea9553979987211d523d27256ea50e9ac2cd48b6
rosarux <seungwoo.ha@gmail.com>
2019-10-02 09:21:36 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 18742,
-	"created_at": "Tue Oct 01 00:21:35 +0000 2019",
+	"tweets": 18743,
+	"created_at": "Wed Oct 02 00:21:35 +0000 2019",
 	"lang": "ja"
 }
---
+++ data/js/tweets/2019_10.js
@@ -0,0 +1,296 @@
+Grailbird.data.tweets_2019_10 = 
+[
+	{
+		"created_at": "Tue Oct 01 10:56:13 +0000 2019",
+		"id": 1178986972590694400,
+		"id_str": "1178986972590694400",
+		"full_text": "다른 앵글 https://t.co/MlUyNLAJFi",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			5
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/MlUyNLAJFi",
+					"expanded_url": "https://twitter.com/rebeccaccc34/status/1178977567111401473",
+					"display_url": "twitter.com/rebeccaccc34/s…",
+					"indices": [
+						6,
+						29
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 94077994,
+			"id_str": "94077994",
+			"name": "縄",
+			"screen_name": "kawanoboru",
+			"location": "마음 만은 언제나 오키나와",
+			"description": "沖縄戦後史を勉強してた、ソウルに住んでいる普通の会社員 오키나와 전후사를 공부한 적이 있는, 서울에 살고 있는 평범한 회사원",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 572,
+			"friends_count": 496,
+			"listed_count": 22,
+			"created_at": "Wed Dec 02 12:19:58 +0000 2009",
+			"favourites_count": 2542,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 17750,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": true,
+			"profile_background_color": "B2DFDA",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme13/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme13/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/983561990789521409/yByV3Crc_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/983561990789521409/yByV3Crc_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/94077994/1441838938",
+			"profile_link_color": "93A644",
+			"profile_sidebar_border_color": "EEEEEE",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1178977567111401500,
+		"quoted_status_id_str": "1178977567111401473",
+		"quoted_status_permalink": {
+			"url": "https://t.co/MlUyNLAJFi",
+			"expanded": "https://twitter.com/rebeccaccc34/status/1178977567111401473",
+			"display": "twitter.com/rebeccaccc34/s…"
+		},
+		"quoted_status": {
+			"created_at": "Tue Oct 01 10:18:51 +0000 2019",
+			"id": 1178977567111401500,
+			"id_str": "1178977567111401473",
+			"full_text": "An online video shows the moment when the police officer aimed his gun at an extreamly close distance in Tsuen Wan, and the protester collapsed onto the pavement approximately 1 second after. Police information confirmed that a protester had been injured by a live round.\n#SOSHK https://t.co/LuApgwUOtK",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				278
+			],
+			"entities": {
+				"hashtags": [
+					{
+						"text": "SOSHK",
+						"indices": [
+							272,
+							278
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 1178977517060808700,
+						"id_str": "1178977517060808704",
+						"indices": [
+							279,
+							302
+						],
+						"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1178977517060808704/pu/img/A27hIZnqwspkBcn5.jpg",
+						"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1178977517060808704/pu/img/A27hIZnqwspkBcn5.jpg",
+						"url": "https://t.co/LuApgwUOtK",
+						"display_url": "pic.twitter.com/LuApgwUOtK",
+						"expanded_url": "https://twitter.com/rebeccaccc34/status/1178977567111401473/video/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 640,
+								"h": 352,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 640,
+								"h": 352,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 640,
+								"h": 352,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1178977517060808700,
+						"id_str": "1178977517060808704",
+						"indices": [
+							279,
+							302
+						],
+						"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1178977517060808704/pu/img/A27hIZnqwspkBcn5.jpg",
+						"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1178977517060808704/pu/img/A27hIZnqwspkBcn5.jpg",
+						"url": "https://t.co/LuApgwUOtK",
+						"display_url": "pic.twitter.com/LuApgwUOtK",
+						"expanded_url": "https://twitter.com/rebeccaccc34/status/1178977567111401473/video/1",
+						"type": "video",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 640,
+								"h": 352,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 640,
+								"h": 352,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 640,
+								"h": 352,
+								"resize": "fit"
+							}
+						},
+						"video_info": {
+							"aspect_ratio": [
+								20,
+								11
+							],
+							"duration_millis": 30280,
+							"variants": [
+								{
+									"content_type": "application/x-mpegURL",
+									"url": "https://video.twimg.com/ext_tw_video/1178977517060808704/pu/pl/GzT8XGt4F1THM_Eh.m3u8?tag=10"
+								},
+								{
+									"bitrate": 256000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/ext_tw_video/1178977517060808704/pu/vid/490x270/Prx2bwSYAf3FWYby.mp4?tag=10"
+								},
+								{
+									"bitrate": 832000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/ext_tw_video/1178977517060808704/pu/vid/640x352/kGhaeiiT9vbBqrcJ.mp4?tag=10"
+								}
+							]
+						},
+						"additional_media_info": {
+							"monetizable": false
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 1169275649858490400,
+				"id_str": "1169275649858490369",
+				"name": "eyesonhkpls",
+				"screen_name": "rebeccaccc34",
+				"location": "",
+				"description": "#followbackhongkong 絕不自殺 煲底之約 all photos are from internet",
+				"url": null,
+				"entities": {
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 87,
+				"friends_count": 178,
+				"listed_count": 0,
+				"created_at": "Wed Sep 04 15:47:07 +0000 2019",
+				"favourites_count": 471,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 281,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "F5F8FA",
+				"profile_background_image_url": null,
+				"profile_background_image_url_https": null,
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1178201015054528513/bRBbOia7_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1178201015054528513/bRBbOia7_normal.jpg",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 281,
+			"favorite_count": 180,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "다른 앵글 https://t.co/MlUyNLAJFi"
+	}
+]
