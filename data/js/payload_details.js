6a6a5c2f19bdfe6684c057b9891865d97a694e12
rosarux <seungwoo.ha@gmail.com>
2019-10-10 09:21:36 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 18775,
-	"created_at": "Wed Oct 09 00:21:35 +0000 2019",
+	"tweets": 18776,
+	"created_at": "Thu Oct 10 00:21:35 +0000 2019",
 	"lang": "ja"
 }
--- data/js/tweets/2019_10.js
+++ data/js/tweets/2019_10.js
@@ -1,6 +1,300 @@
 Grailbird.data.tweets_2019_10 = 
 [
 	{
+		"created_at": "Wed Oct 09 11:05:05 +0000 2019",
+		"id": 1181888154082480000,
+		"id_str": "1181888308936265728",
+		"full_text": "ネコのグレさん。すっかり大人になり、最近は、ワンコ達とのお散歩に参加をしてくれなくなり、少し寂しかったのですが。。。\nここ数日は、お散歩熱が復活！かなりのペースでご一緒をしております。\nでも、もう少し早く歩いてくれると嬉しいんだけどなぁ。。。\n#猫好き 　#お散歩 https://t.co/ZGyI1UHcXP",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			132
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "猫好き",
+					"indices": [
+						122,
+						126
+					]
+				},
+				{
+					"text": "お散歩",
+					"indices": [
+						128,
+						132
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1181888017226588200,
+					"id_str": "1181888017226588160",
+					"indices": [
+						133,
+						156
+					],
+					"media_url": "http://pbs.twimg.com/media/EGbo48cU4AAHj9H.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EGbo48cU4AAHj9H.jpg",
+					"url": "https://t.co/ZGyI1UHcXP",
+					"display_url": "pic.twitter.com/ZGyI1UHcXP",
+					"expanded_url": "https://twitter.com/furaisou/status/1181888154082480130/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1000,
+							"h": 750,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1000,
+							"h": 750,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1181888017226588200,
+					"id_str": "1181888017226588160",
+					"indices": [
+						133,
+						156
+					],
+					"media_url": "http://pbs.twimg.com/media/EGbo48cU4AAHj9H.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EGbo48cU4AAHj9H.jpg",
+					"url": "https://t.co/ZGyI1UHcXP",
+					"display_url": "pic.twitter.com/ZGyI1UHcXP",
+					"expanded_url": "https://twitter.com/furaisou/status/1181888154082480130/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1000,
+							"h": 750,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1000,
+							"h": 750,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1181888050416062500,
+					"id_str": "1181888050416062464",
+					"indices": [
+						133,
+						156
+					],
+					"media_url": "http://pbs.twimg.com/media/EGbo64FUEAAJjIW.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EGbo64FUEAAJjIW.jpg",
+					"url": "https://t.co/ZGyI1UHcXP",
+					"display_url": "pic.twitter.com/ZGyI1UHcXP",
+					"expanded_url": "https://twitter.com/furaisou/status/1181888154082480130/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1000,
+							"h": 750,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1000,
+							"h": 750,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1181888082645176300,
+					"id_str": "1181888082645176320",
+					"indices": [
+						133,
+						156
+					],
+					"media_url": "http://pbs.twimg.com/media/EGbo8wJVUAA7OTo.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EGbo8wJVUAA7OTo.jpg",
+					"url": "https://t.co/ZGyI1UHcXP",
+					"display_url": "pic.twitter.com/ZGyI1UHcXP",
+					"expanded_url": "https://twitter.com/furaisou/status/1181888154082480130/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1000,
+							"h": 750,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1000,
+							"h": 750,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 207791068,
+			"id_str": "207791068",
+			"name": "風来荘",
+			"screen_name": "furaisou",
+			"location": "沖縄県　今帰仁村",
+			"description": "13年前に鎌倉から沖縄へ。  。。                                           海のそばで『風来荘　fu-rai-sou』 と言う名の宿を営んでおります.朝食には国産小麦を使って焼き上げる自家製天然酵母パンとスープでほっこりと。。ゆったりとした沖縄時間をお楽しみ下さい。。",
+			"url": "http://t.co/q1DVIfauZK",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/q1DVIfauZK",
+							"expanded_url": "http://www7.plala.or.jp/fu-rai-sou/",
+							"display_url": "www7.plala.or.jp/fu-rai-sou/",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 600,
+			"friends_count": 278,
+			"listed_count": 15,
+			"created_at": "Tue Oct 26 01:28:07 +0000 2010",
+			"favourites_count": 1677,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 1081,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "131516",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/439958135634001921/P1j1yf6a_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/439958135634001921/P1j1yf6a_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/207791068/1398257915",
+			"profile_link_color": "256DD9",
+			"profile_sidebar_border_color": "EEEEEE",
+			"profile_sidebar_fill_color": "EFEFEF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": {
+			"id": "052e049119fd8da1",
+			"url": "https://api.twitter.com/1.1/geo/id/052e049119fd8da1.json",
+			"place_type": "admin",
+			"name": "沖縄",
+			"full_name": "日本 沖縄",
+			"country_code": "JP",
+			"country": "日本",
+			"contained_within": [],
+			"bounding_box": {
+				"type": "Polygon",
+				"coordinates": [
+					[
+						[
+							122.933784,
+							24.045669
+						],
+						[
+							131.332071,
+							24.045669
+						],
+						[
+							131.332071,
+							27.095384
+						],
+						[
+							122.933784,
+							27.095384
+						]
+					]
+				]
+			},
+			"attributes": {}
+		},
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 12,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ja",
+		"text": "ネコのグレさん。すっかり大人になり、最近は、ワンコ達とのお散歩に参加をしてくれなくなり、少し寂しかったのですが。。。\nここ数日は、お散歩熱が復活！かなりのペースでご一緒をしております。\nでも、もう少し早く歩いてくれると嬉しいんだけどなぁ。。。\n#猫好き 　#お散歩 https://t.co/ZGyI1UHcXP"
+	},
+	{
 		"created_at": "Tue Oct 08 13:47:14 +0000 2019",
 		"id": 1181133806129959000,
 		"id_str": "1181566724035903488",
