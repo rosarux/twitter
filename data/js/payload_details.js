df9924c93602ab43fd59a685f8ed4a82818bd4a6
rosarux <seungwoo.ha@gmail.com>
2020-01-02 09:21:36 +0900

    Added 3 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 19042,
-	"created_at": "Wed Jan 01 00:21:35 +0000 2020",
+	"tweets": 19045,
+	"created_at": "Thu Jan 02 00:21:35 +0000 2020",
 	"lang": "ja"
 }
---
+++ data/js/tweets/2020_01.js
@@ -0,0 +1,645 @@
+Grailbird.data.tweets_2020_01 = 
+[
+	{
+		"created_at": "Wed Jan 01 13:16:36 +0000 2020",
+		"id": 1212336309475455000,
+		"id_str": "1212361986215825411",
+		"full_text": "4차 산업 혁명은 IoT, 로봇 공학, VR, AI 같은것을 포진 하고 있는데 이걸 응용해서 쳐나오는게 자꾸 우버 클론 같은  일용직 브로커들쟁이 밖에 없는 웃픈 상황",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			93
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/#!/download/ipad\" rel=\"nofollow\">Twitter for iPad</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 806820326906675200,
+			"id_str": "806820326906675200",
+			"name": "KJ",
+			"screen_name": "isotoxin390",
+			"location": "",
+			"description": "",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 976,
+			"friends_count": 40,
+			"listed_count": 13,
+			"created_at": "Thu Dec 08 11:18:50 +0000 2016",
+			"favourites_count": 3178,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 35994,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://abs.twimg.com/sticky/default_profile_images/default_profile_normal.png",
+			"profile_image_url_https": "https://abs.twimg.com/sticky/default_profile_images/default_profile_normal.png",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": true,
+			"can_media_tag": true,
+			"followed_by": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 11,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "ko",
+		"text": "4차 산업 혁명은 IoT, 로봇 공학, VR, AI 같은것을 포진 하고 있는데 이걸 응용해서 쳐나오는게 자꾸 우버 클론 같은  일용직 브로커들쟁이 밖에 없는 웃픈 상황"
+	},
+	{
+		"created_at": "Wed Jan 01 11:50:59 +0000 2020",
+		"id": 1212317395445932000,
+		"id_str": "1212340439417704449",
+		"full_text": "初興し（ハチウクシー）＠辺野古松田ぬ浜。\n\n雲の合間から初日が差し込む様子は天上界と通じているような神秘的な光景でした。2020年、今年こそは辺野古新基地建設を中止させ、この海を本来の姿に戻しましょう！ https://t.co/lndrMoQ5md",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			101
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1212317379851481000,
+					"id_str": "1212317379851481088",
+					"indices": [
+						102,
+						125
+					],
+					"media_url": "http://pbs.twimg.com/media/ENMEO90UcAAT6eI.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ENMEO90UcAAT6eI.jpg",
+					"url": "https://t.co/lndrMoQ5md",
+					"display_url": "pic.twitter.com/lndrMoQ5md",
+					"expanded_url": "https://twitter.com/yanbarugurashi/status/1212317395445932032/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1200,
+							"h": 800,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1366,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"medium": {
+							"faces": [
+								{
+									"x": 329,
+									"y": 468,
+									"h": 58,
+									"w": 58
+								}
+							]
+						},
+						"orig": {
+							"faces": [
+								{
+									"x": 1124,
+									"y": 1600,
+									"h": 200,
+									"w": 200
+								}
+							]
+						},
+						"small": {
+							"faces": [
+								{
+									"x": 186,
+									"y": 265,
+									"h": 33,
+									"w": 33
+								}
+							]
+						},
+						"large": {
+							"faces": [
+								{
+									"x": 562,
+									"y": 800,
+									"h": 100,
+									"w": 100
+								}
+							]
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1212317379851481000,
+					"id_str": "1212317379851481088",
+					"indices": [
+						102,
+						125
+					],
+					"media_url": "http://pbs.twimg.com/media/ENMEO90UcAAT6eI.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ENMEO90UcAAT6eI.jpg",
+					"url": "https://t.co/lndrMoQ5md",
+					"display_url": "pic.twitter.com/lndrMoQ5md",
+					"expanded_url": "https://twitter.com/yanbarugurashi/status/1212317395445932032/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1200,
+							"h": 800,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1366,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"medium": {
+							"faces": [
+								{
+									"x": 329,
+									"y": 468,
+									"h": 58,
+									"w": 58
+								}
+							]
+						},
+						"orig": {
+							"faces": [
+								{
+									"x": 1124,
+									"y": 1600,
+									"h": 200,
+									"w": 200
+								}
+							]
+						},
+						"small": {
+							"faces": [
+								{
+									"x": 186,
+									"y": 265,
+									"h": 33,
+									"w": 33
+								}
+							]
+						},
+						"large": {
+							"faces": [
+								{
+									"x": 562,
+									"y": 800,
+									"h": 100,
+									"w": 100
+								}
+							]
+						}
+					}
+				},
+				{
+					"id": 1212317379864084500,
+					"id_str": "1212317379864084481",
+					"indices": [
+						102,
+						125
+					],
+					"media_url": "http://pbs.twimg.com/media/ENMEO93UwAERWLu.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ENMEO93UwAERWLu.jpg",
+					"url": "https://t.co/lndrMoQ5md",
+					"display_url": "pic.twitter.com/lndrMoQ5md",
+					"expanded_url": "https://twitter.com/yanbarugurashi/status/1212317395445932032/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1365,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 800,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"small": {
+							"faces": []
+						},
+						"large": {
+							"faces": []
+						},
+						"orig": {
+							"faces": []
+						},
+						"medium": {
+							"faces": []
+						}
+					}
+				},
+				{
+					"id": 1212317379859894300,
+					"id_str": "1212317379859894273",
+					"indices": [
+						102,
+						125
+					],
+					"media_url": "http://pbs.twimg.com/media/ENMEO92U0AEwW3e.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ENMEO92U0AEwW3e.jpg",
+					"url": "https://t.co/lndrMoQ5md",
+					"display_url": "pic.twitter.com/lndrMoQ5md",
+					"expanded_url": "https://twitter.com/yanbarugurashi/status/1212317395445932032/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1366,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 800,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"small": {
+							"faces": []
+						},
+						"orig": {
+							"faces": []
+						},
+						"large": {
+							"faces": []
+						},
+						"medium": {
+							"faces": []
+						}
+					}
+				},
+				{
+					"id": 1212317379855667200,
+					"id_str": "1212317379855667201",
+					"indices": [
+						102,
+						125
+					],
+					"media_url": "http://pbs.twimg.com/media/ENMEO91UUAEQFVJ.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ENMEO91UUAEQFVJ.jpg",
+					"url": "https://t.co/lndrMoQ5md",
+					"display_url": "pic.twitter.com/lndrMoQ5md",
+					"expanded_url": "https://twitter.com/yanbarugurashi/status/1212317395445932032/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 800,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1366,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"orig": {
+							"faces": [
+								{
+									"x": 162,
+									"y": 1341,
+									"h": 227,
+									"w": 227
+								},
+								{
+									"x": 106,
+									"y": 1076,
+									"h": 317,
+									"w": 317
+								}
+							]
+						},
+						"medium": {
+							"faces": [
+								{
+									"x": 55,
+									"y": 455,
+									"h": 77,
+									"w": 77
+								},
+								{
+									"x": 36,
+									"y": 365,
+									"h": 107,
+									"w": 107
+								}
+							]
+						},
+						"large": {
+							"faces": [
+								{
+									"x": 93,
+									"y": 777,
+									"h": 131,
+									"w": 131
+								},
+								{
+									"x": 61,
+									"y": 623,
+									"h": 183,
+									"w": 183
+								}
+							]
+						},
+						"small": {
+							"faces": [
+								{
+									"x": 31,
+									"y": 258,
+									"h": 43,
+									"w": 43
+								},
+								{
+									"x": 20,
+									"y": 207,
+									"h": 61,
+									"w": 61
+								}
+							]
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 773324146709102600,
+			"id_str": "773324146709102592",
+			"name": "やんばるぐらし",
+			"screen_name": "yanbarugurashi",
+			"location": "沖縄県名護市",
+			"description": "沖縄県名護市。足を運んでこの目で見て、話して聞いて初めて分かることがある。交流しましょう。話しましょう。美ら海と希少生物の宝庫やんばるの森を守りたい。辺野古新基地建設反対、高江ヘリパッド運用反対。整備集約→❌無条件返還→⭕何が何でも沖縄に基地を作りたい日本政府に真っ向反発。非暴力不服従直接行動。いつでも愛とユーモアを。",
+			"url": "https://t.co/Duo5H4zefK",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Duo5H4zefK",
+							"expanded_url": "http://yanbarugurashiyanbarugurashi.hatenablog.com",
+							"display_url": "…ugurashiyanbarugurashi.hatenablog.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 5965,
+			"friends_count": 695,
+			"listed_count": 193,
+			"created_at": "Wed Sep 07 00:56:59 +0000 2016",
+			"favourites_count": 125924,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 9611,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/775404140901961728/f4ViwMfG_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/775404140901961728/f4ViwMfG_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/773324146709102592/1473706410",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 101,
+		"favorite_count": 184,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ja",
+		"text": "初興し（ハチウクシー）＠辺野古松田ぬ浜。\n\n雲の合間から初日が差し込む様子は天上界と通じているような神秘的な光景でした。2020年、今年こそは辺野古新基地建設を中止させ、この海を本来の姿に戻しましょう！ https://t.co/lndrMoQ5md"
+	},
+	{
+		"created_at": "Wed Jan 01 11:48:06 +0000 2020",
+		"id": 1212336559728423000,
+		"id_str": "1212339712649613312",
+		"full_text": "참, 2020부터라도 만 나이 셉시다. 이유는 작년에 구구절절 쓴 적이 있어요. 1월 1월에 한 살 더 먹는 나이는 틀린 겁니다. 법적으로나, 신체적으로나...",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			89
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 297930927,
+			"id_str": "297930927",
+			"name": "s.lee",
+			"screen_name": "sunbaelee",
+			"location": "Hong Kong • Seoul",
+			"description": "creative consultant·writer \n*[선택하지 않을 자유]가 나왔습니다. 그 외 [서른엔 뭐라도 되어 있을 줄 알았다], [멋진 사람들의 물건](구 [쇼핑의 기술]), [잇 걸], [맨즈 잇 스타일], [잇 코스메틱] 등 저서가 있습니다.",
+			"url": "https://t.co/FEv6TyOVmd",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/FEv6TyOVmd",
+							"expanded_url": "https://instagram.com/askitgirl",
+							"display_url": "instagram.com/askitgirl",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 62836,
+			"friends_count": 823,
+			"listed_count": 244,
+			"created_at": "Fri May 13 11:16:07 +0000 2011",
+			"favourites_count": 1006,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 34451,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "233838",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme13/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme13/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1030298632036081664/Is7Ktkoj_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1030298632036081664/Is7Ktkoj_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/297930927/1534476960",
+			"profile_link_color": "80796A",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "20201E",
+			"profile_text_color": "C8A86F",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": false,
+			"followed_by": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 41,
+		"favorite_count": 40,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "ko",
+		"text": "참, 2020부터라도 만 나이 셉시다. 이유는 작년에 구구절절 쓴 적이 있어요. 1월 1월에 한 살 더 먹는 나이는 틀린 겁니다. 법적으로나, 신체적으로나..."
+	}
+]
