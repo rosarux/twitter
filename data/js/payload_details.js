3261fae6e7374fc27cf070381c510020111ac78a
rosarux <seungwoo.ha@gmail.com>
2020-02-02 09:21:36 +0900

    Added 4 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 19116,
-	"created_at": "Fri Jan 31 00:21:35 +0000 2020",
+	"tweets": 19120,
+	"created_at": "Sun Feb 02 00:21:35 +0000 2020",
 	"lang": "ja"
 }
---
+++ data/js/tweets/2020_02.js
@@ -0,0 +1,643 @@
+Grailbird.data.tweets_2020_02 = 
+[
+	{
+		"created_at": "Sat Feb 01 05:19:37 +0000 2020",
+		"id": 1223196697544708000,
+		"id_str": "1223475973342912512",
+		"full_text": "타임라인을 보다 조금 답답한 생각이 들어 한마디 하자면.. \n\n확진자의 동선을 공개하는 이유는 내용을 보는 우리들이 그 동선에 포함되었는지를 확인할 수 있게 하기위함이지 그 사람이 무슨 생활을 하는 사람인지, 왜 늦었는지를 알아보기 위함이 아니에요.",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			139
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 716984632722391000,
+			"id_str": "716984632722391042",
+			"name": "🎗아웃사이더 곰(熊)",
+			"screen_name": "Iorek_the_bear",
+			"location": "뒤를 돌아봐...",
+			"description": "진짜 아웃사이더임. 화성에 가고 싶습니다.\n트친님들이 팔로우할 만큼 좋은 사람이 아닙니다.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1057,
+			"friends_count": 650,
+			"listed_count": 15,
+			"created_at": "Mon Apr 04 13:43:52 +0000 2016",
+			"favourites_count": 9301,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 107582,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1153427800382005248/S42fEdKM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1153427800382005248/S42fEdKM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/716984632722391042/1459777587",
+			"profile_link_color": "981CEB",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1236,
+		"favorite_count": 453,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "ko",
+		"text": "타임라인을 보다 조금 답답한 생각이 들어 한마디 하자면.. \n\n확진자의 동선을 공개하는 이유는 내용을 보는 우리들이 그 동선에 포함되었는지를 확인할 수 있게 하기위함이지 그 사람이 무슨 생활을 하는 사람인지, 왜 늦었는지를 알아보기 위함이 아니에요."
+	},
+	{
+		"created_at": "Sat Feb 01 04:08:05 +0000 2020",
+		"id": 1223445626148573200,
+		"id_str": "1223457969532047360",
+		"full_text": "“First Do No Harm.” The Impact of Sanctions on Public Health in North Korea\n\nKee B. Park and Haeyoung Kim \n\nhttps://t.co/U5pwt6e3tP\n\nCc: @WomenCrossDMZ https://t.co/uxZGF4PwT5",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			151
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "WomenCrossDMZ",
+					"name": "WomenCrossDMZ",
+					"id": 2431166317,
+					"id_str": "2431166317",
+					"indices": [
+						137,
+						151
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/U5pwt6e3tP",
+					"expanded_url": "https://apjjf.org/2020/2/Park.html",
+					"display_url": "apjjf.org/2020/2/Park.ht…",
+					"indices": [
+						108,
+						131
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1223445624424747000,
+					"id_str": "1223445624424747008",
+					"indices": [
+						152,
+						175
+					],
+					"media_url": "http://pbs.twimg.com/media/EPqNUDXX0AALHrR.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EPqNUDXX0AALHrR.jpg",
+					"url": "https://t.co/uxZGF4PwT5",
+					"display_url": "pic.twitter.com/uxZGF4PwT5",
+					"expanded_url": "https://twitter.com/apjjf/status/1223445626148573184/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 800,
+							"h": 533,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 800,
+							"h": 533,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"medium": {
+							"faces": []
+						},
+						"small": {
+							"faces": []
+						},
+						"large": {
+							"faces": []
+						},
+						"orig": {
+							"faces": []
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1223445624424747000,
+					"id_str": "1223445624424747008",
+					"indices": [
+						152,
+						175
+					],
+					"media_url": "http://pbs.twimg.com/media/EPqNUDXX0AALHrR.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EPqNUDXX0AALHrR.jpg",
+					"url": "https://t.co/uxZGF4PwT5",
+					"display_url": "pic.twitter.com/uxZGF4PwT5",
+					"expanded_url": "https://twitter.com/apjjf/status/1223445626148573184/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 800,
+							"h": 533,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 800,
+							"h": 533,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"medium": {
+							"faces": []
+						},
+						"small": {
+							"faces": []
+						},
+						"large": {
+							"faces": []
+						},
+						"orig": {
+							"faces": []
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://buffer.com\" rel=\"nofollow\">Buffer</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 105521430,
+			"id_str": "105521430",
+			"name": "Asia-Pacific Journal",
+			"screen_name": "apjjf",
+			"location": "Global",
+			"description": "In-depth critical analysis of the forces shaping the Asia-Pacific region & the world.　RTs, follows, mentions, listing, etc. are not endorsements.",
+			"url": "https://t.co/GRfPGHSEb4",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/GRfPGHSEb4",
+							"expanded_url": "http://www.apjjf.org",
+							"display_url": "apjjf.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 7324,
+			"friends_count": 774,
+			"listed_count": 518,
+			"created_at": "Sat Jan 16 16:11:14 +0000 2010",
+			"favourites_count": 13622,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 14067,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F6F1DF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme19/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme19/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/637198771/1284_bird_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/637198771/1284_bird_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/105521430/1555430110",
+			"profile_link_color": "572E4F",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "EE0B5B",
+			"profile_text_color": "027B7F",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": true,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 6,
+		"favorite_count": 8,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "“First Do No Harm.” The Impact of Sanctions on Public Health in North Korea\n\nKee B. Park and Haeyoung Kim \n\nhttps://t.co/U5pwt6e3tP\n\nCc: @WomenCrossDMZ https://t.co/uxZGF4PwT5"
+	},
+	{
+		"created_at": "Sat Feb 01 01:20:21 +0000 2020",
+		"id": 1222992639940644900,
+		"id_str": "1223415757809176576",
+		"full_text": "\"사실을 가까이 하고 공포를 멀리 하십시오. \n과학을 가까이 하고 루머를 멀리 하십시오. \n연대를 가까이 하고 낙인을 멀리 하십시오.\"\nWHO 총장 테드로스 아드하놈 게브레예수스 https://t.co/Nbhoq4wKfr",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			99
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/Nbhoq4wKfr",
+					"expanded_url": "https://twitter.com/drtedros/status/1222982871444598789",
+					"display_url": "twitter.com/drtedros/statu…",
+					"indices": [
+						100,
+						123
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1220858795770822700,
+			"id_str": "1220858795770822657",
+			"name": "신종 코로나바이러스 뉴스봇",
+			"screen_name": "2019_nCoV_ko",
+			"location": "2019nCoV@mail.com",
+			"description": "#신종코로나바이러스 (2019-nCoV)에 관한 정보를 전달합니다. ‘우한폐렴’이라는 명칭을 사용하지 않습니다. 감염병 예방을 위해 #손씻기 를 생활화합시다. 패닉에 빠지지 마십시오. 침착하고 정확한 정보를 공유하십시오.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 15112,
+			"friends_count": 40,
+			"listed_count": 16,
+			"created_at": "Sat Jan 25 00:00:15 +0000 2020",
+			"favourites_count": 7,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 438,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1220985009714036736/HMP2IAEd_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1220985009714036736/HMP2IAEd_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1220858795770822657/1580288637",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"can_media_tag": false,
+			"followed_by": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1222982871444598800,
+		"quoted_status_id_str": "1222982871444598789",
+		"quoted_status_permalink": {
+			"url": "https://t.co/Nbhoq4wKfr",
+			"expanded": "https://twitter.com/drtedros/status/1222982871444598789",
+			"display": "twitter.com/drtedros/statu…"
+		},
+		"quoted_status": {
+			"created_at": "Thu Jan 30 20:40:13 +0000 2020",
+			"id": 1222982871444598800,
+			"id_str": "1222982871444598789",
+			"full_text": "We must act now to help countries prepare for that possibility. \n\nThis is the time for facts, not fear. \n\nThis is the time for science, not rumours.\n\nThis is the time for solidarity, not stigma.\n\n#2019nCoV \n\nhttps://t.co/nda2uf2ExC",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				231
+			],
+			"entities": {
+				"hashtags": [
+					{
+						"text": "2019nCoV",
+						"indices": [
+							196,
+							205
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/nda2uf2ExC",
+						"expanded_url": "https://twitter.com/WHO/status/1222969618505093121?s=20",
+						"display_url": "twitter.com/WHO/status/122…",
+						"indices": [
+							208,
+							231
+						]
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": 1222982869871669200,
+			"in_reply_to_status_id_str": "1222982869871669251",
+			"in_reply_to_user_id": 189868631,
+			"in_reply_to_user_id_str": "189868631",
+			"in_reply_to_screen_name": "DrTedros",
+			"user": {
+				"id": 189868631,
+				"id_str": "189868631",
+				"name": "Tedros Adhanom Ghebreyesus",
+				"screen_name": "DrTedros",
+				"location": "Geneva, Switzerland",
+				"description": "Director-General of the World Health Organization",
+				"url": "https://t.co/wVulKuROWG",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/wVulKuROWG",
+								"expanded_url": "http://www.who.int",
+								"display_url": "who.int",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 465140,
+				"friends_count": 871,
+				"listed_count": 1177,
+				"created_at": "Sun Sep 12 13:09:27 +0000 2010",
+				"favourites_count": 6853,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": true,
+				"statuses_count": 21048,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "C0DEED",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1220449347239927829/NqD1T5B9_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1220449347239927829/NqD1T5B9_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/189868631/1569115668",
+				"profile_link_color": "0084B4",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"can_media_tag": true,
+				"followed_by": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": true,
+			"quoted_status_id": 1222969618505093000,
+			"quoted_status_id_str": "1222969618505093121",
+			"quoted_status_permalink": {
+				"url": "https://t.co/nda2uf2ExC",
+				"expanded": "https://twitter.com/WHO/status/1222969618505093121?s=20",
+				"display": "twitter.com/WHO/status/122…"
+			},
+			"retweet_count": 1300,
+			"favorite_count": 1763,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 1990,
+		"favorite_count": 654,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "\"사실을 가까이 하고 공포를 멀리 하십시오. \n과학을 가까이 하고 루머를 멀리 하십시오. \n연대를 가까이 하고 낙인을 멀리 하십시오.\"\nWHO 총장 테드로스 아드하놈 게브레예수스 https://t.co/Nbhoq4wKfr"
+	},
+	{
+		"created_at": "Sat Feb 01 00:27:08 +0000 2020",
+		"id": 1221293030990180400,
+		"id_str": "1223402368282198016",
+		"full_text": "“우리가 맞서 싸워야 할 것은 가난과 질병이지, 가난한 사람과 병든 사람이 아닙니다”",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			47
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 767327335179690000,
+			"id_str": "767327335179689985",
+			"name": "Gabriel",
+			"screen_name": "WaywardMorpheus",
+			"location": "",
+			"description": "나와 상관없는 이야기 https://t.co/x6k7u7HhA6",
+			"url": "https://t.co/qQAhAzBuCD",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/qQAhAzBuCD",
+							"expanded_url": "https://www.cubi.so/",
+							"display_url": "cubi.so",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/x6k7u7HhA6",
+							"expanded_url": "http://asked.kr/morpheus01",
+							"display_url": "asked.kr/morpheus01",
+							"indices": [
+								12,
+								35
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 1530,
+			"friends_count": 337,
+			"listed_count": 5,
+			"created_at": "Sun Aug 21 11:47:47 +0000 2016",
+			"favourites_count": 18044,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 40946,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1215492157458374656/YYhK_3NM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1215492157458374656/YYhK_3NM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/767327335179689985/1539434130",
+			"profile_link_color": "F58EA8",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": false,
+			"followed_by": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 11521,
+		"favorite_count": 5407,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "ko",
+		"text": "“우리가 맞서 싸워야 할 것은 가난과 질병이지, 가난한 사람과 병든 사람이 아닙니다”"
+	}
+]
