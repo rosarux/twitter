6580e4038a552dd672e7d45d898c49aa6d54bbfc
rosarux <seungwoo.ha@gmail.com>
2020-02-17 09:21:36 +0900

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 19165,
-	"created_at": "Sun Feb 16 00:21:35 +0000 2020",
+	"tweets": 19167,
+	"created_at": "Mon Feb 17 00:21:35 +0000 2020",
 	"lang": "ja"
 }
--- data/js/tweets/2020_02.js
+++ data/js/tweets/2020_02.js
@@ -1,6 +1,283 @@
 Grailbird.data.tweets_2020_02 = 
 [
 	{
+		"created_at": "Sun Feb 16 13:33:15 +0000 2020",
+		"id": 1229032216807624700,
+		"id_str": "1229036018222825472",
+		"full_text": "“XX, XXXX, XXXX 등 이쪽 계열 음식점의 특징. 우선 그럴싸한 음식을 평균 이상의 가격과 평균 이상의 퀄리티로 런칭한다. 고객 모시고 먹기에 쪽팔리지 않은 퀄리티에 딱 법카 긁기 좋은 정도의 미묘한 가격이 핵심 경쟁력. 자리를 잡으면 사정없이 퀄리티를 후려친다.” 식당 유지 참 어려운가보다.",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			169
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 133255001,
+			"id_str": "133255001",
+			"name": "Tor Sean",
+			"screen_name": "TorSean",
+			"location": "",
+			"description": "아름다움이 악함을 이긴다.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1179,
+			"friends_count": 359,
+			"listed_count": 25,
+			"created_at": "Thu Apr 15 11:51:23 +0000 2010",
+			"favourites_count": 822,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 19441,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/733255325600505857/EMTpAXiY_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/733255325600505857/EMTpAXiY_normal.jpg",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 7,
+		"favorite_count": 4,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "ko",
+		"text": "“XX, XXXX, XXXX 등 이쪽 계열 음식점의 특징. 우선 그럴싸한 음식을 평균 이상의 가격과 평균 이상의 퀄리티로 런칭한다. 고객 모시고 먹기에 쪽팔리지 않은 퀄리티에 딱 법카 긁기 좋은 정도의 미묘한 가격이 핵심 경쟁력. 자리를 잡으면 사정없이 퀄리티를 후려친다.” 식당 유지 참 어려운가보다."
+	},
+	{
+		"created_at": "Sun Feb 16 03:57:39 +0000 2020",
+		"id": 1228678715413942300,
+		"id_str": "1228891161634332672",
+		"full_text": "쿨한 MB 2컷 https://t.co/9SvRP8gBqR",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			8
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1228678671424094200,
+					"id_str": "1228678671424094208",
+					"indices": [
+						9,
+						32
+					],
+					"media_url": "http://pbs.twimg.com/media/EQ0kvelVAAAENYY.png",
+					"media_url_https": "https://pbs.twimg.com/media/EQ0kvelVAAAENYY.png",
+					"url": "https://t.co/9SvRP8gBqR",
+					"display_url": "pic.twitter.com/9SvRP8gBqR",
+					"expanded_url": "https://twitter.com/kbkgwyd/status/1228678715413942272/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 413,
+							"h": 308,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 413,
+							"h": 308,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 413,
+							"h": 308,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1228678671424094200,
+					"id_str": "1228678671424094208",
+					"indices": [
+						9,
+						32
+					],
+					"media_url": "http://pbs.twimg.com/media/EQ0kvelVAAAENYY.png",
+					"media_url_https": "https://pbs.twimg.com/media/EQ0kvelVAAAENYY.png",
+					"url": "https://t.co/9SvRP8gBqR",
+					"display_url": "pic.twitter.com/9SvRP8gBqR",
+					"expanded_url": "https://twitter.com/kbkgwyd/status/1228678715413942272/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 413,
+							"h": 308,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 413,
+							"h": 308,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 413,
+							"h": 308,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				},
+				{
+					"id": 1228678691460284400,
+					"id_str": "1228678691460284417",
+					"indices": [
+						9,
+						32
+					],
+					"media_url": "http://pbs.twimg.com/media/EQ0kwpOVAAEQxiv.png",
+					"media_url_https": "https://pbs.twimg.com/media/EQ0kwpOVAAEQxiv.png",
+					"url": "https://t.co/9SvRP8gBqR",
+					"display_url": "pic.twitter.com/9SvRP8gBqR",
+					"expanded_url": "https://twitter.com/kbkgwyd/status/1228678715413942272/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 428,
+							"h": 240,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 428,
+							"h": 240,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 428,
+							"h": 240,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1734503390,
+			"id_str": "1734503390",
+			"name": "5G(오지)리",
+			"screen_name": "kbkgwyd",
+			"location": "",
+			"description": "깨시&승냥",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 7193,
+			"friends_count": 421,
+			"listed_count": 30,
+			"created_at": "Fri Sep 06 08:47:03 +0000 2013",
+			"favourites_count": 2643,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 55587,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme5/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme5/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/836078918079811584/slJFrVuc_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/836078918079811584/slJFrVuc_normal.jpg",
+			"profile_link_color": "4A913C",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 93,
+		"favorite_count": 11,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "쿨한 MB 2컷 https://t.co/9SvRP8gBqR"
+	},
+	{
 		"created_at": "Sat Feb 15 16:48:11 +0000 2020",
 		"id": 1228695830262976500,
 		"id_str": "1228722687016521728",
