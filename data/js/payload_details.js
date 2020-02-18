e1bf931767318251ea4d9df72547248377e8512e
rosarux <seungwoo.ha@gmail.com>
2020-02-18 09:21:37 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 19167,
-	"created_at": "Mon Feb 17 00:21:35 +0000 2020",
+	"tweets": 19168,
+	"created_at": "Tue Feb 18 00:21:36 +0000 2020",
 	"lang": "ja"
 }
--- data/js/tweets/2020_02.js
+++ data/js/tweets/2020_02.js
@@ -1,6 +1,101 @@
 Grailbird.data.tweets_2020_02 = 
 [
 	{
+		"created_at": "Mon Feb 17 11:59:21 +0000 2020",
+		"id": 1229357965422891000,
+		"id_str": "1229374775119626240",
+		"full_text": "규제가 무슨 요술방망이인줄 아나…\n\"위에 정책이 있으면 아래에는 대책이 있다\"라는 말이 괜히 나오는 것이 아님\n상대도 창의적이기에 성공적인 규제는 사람들이 상상하는 명령 하나로 다 틀어막는 정책이라기보다는, 도망갈 길을 열어놓고 살살 몰되 상대의 반응을 봐가며 조절하는 것에 가까움",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			157
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 262353521,
+			"id_str": "262353521",
+			"name": "sonnet",
+			"screen_name": "quarantineSta",
+			"location": "Seoul, ROK",
+			"description": "이전에 나는 죄가 있기 때문에 사형을 당하거나 감옥에 갇히는 것이라고 생각했다. 그러나 이제는 먼저 누군가에게 ‘괘씸하다’는 판정을 받게 된 연후에 죄를 얻게 되는 것임을 알게 되었다. - 루쉰(魯迅), 「괘씸죄」-",
+			"url": "http://t.co/jHtektmz9j",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/jHtektmz9j",
+							"expanded_url": "http://sonnet.egloos.com/",
+							"display_url": "sonnet.egloos.com",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3936,
+			"friends_count": 268,
+			"listed_count": 124,
+			"created_at": "Mon Mar 07 22:03:27 +0000 2011",
+			"favourites_count": 858,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 11091,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1401335216/hm100px_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1401335216/hm100px_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/262353521/1398770126",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 25,
+		"favorite_count": 13,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "ko",
+		"text": "규제가 무슨 요술방망이인줄 아나…\n\"위에 정책이 있으면 아래에는 대책이 있다\"라는 말이 괜히 나오는 것이 아님\n상대도 창의적이기에 성공적인 규제는 사람들이 상상하는 명령 하나로 다 틀어막는 정책이라기보다는, 도망갈 길을 열어놓고 살살 몰되 상대의 반응을 봐가며 조절하는 것에 가까움"
+	},
+	{
 		"created_at": "Sun Feb 16 13:33:15 +0000 2020",
 		"id": 1229032216807624700,
 		"id_str": "1229036018222825472",
