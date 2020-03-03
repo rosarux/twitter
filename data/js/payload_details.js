6f14e201dcb1ddfee067c81586e112cd7919e806
rosarux <seungwoo.ha@gmail.com>
2020-03-03 09:21:35 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 19246,
-	"created_at": "Mon Mar 02 00:21:35 +0000 2020",
+	"tweets": 19247,
+	"created_at": "Tue Mar 03 00:21:34 +0000 2020",
 	"lang": "ja"
 }
--- data/js/tweets/2020_03.js
+++ data/js/tweets/2020_03.js
@@ -1,6 +1,101 @@
 Grailbird.data.tweets_2020_03 = 
 [
 	{
+		"created_at": "Mon Mar 02 12:42:01 +0000 2020",
+		"id": 1234439771071500300,
+		"id_str": "1234458940022259712",
+		"full_text": "어르신. 이 난리통에 마스크 하나 제대로 분배를 못하냐, 역시 시장주의 경제를 부정하는 정권은 안된다라뇨. 한 가지만 해 주세요.",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			72
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 50653800,
+			"id_str": "50653800",
+			"name": "Raoul Kim / 김형진",
+			"screen_name": "Raoulian",
+			"location": "37.483573,127.087094",
+			"description": "Game Developer / Enuma, inc. ラウルが悪い",
+			"url": "https://t.co/ALa8XtEw71",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/ALa8XtEw71",
+							"expanded_url": "http://enuma.com/",
+							"display_url": "enuma.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3064,
+			"friends_count": 2974,
+			"listed_count": 104,
+			"created_at": "Thu Jun 25 14:05:05 +0000 2009",
+			"favourites_count": 69973,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 66785,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1109509360/n1077128533_3873_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1109509360/n1077128533_3873_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/50653800/1476615743",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 60,
+		"favorite_count": 13,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "ko",
+		"text": "어르신. 이 난리통에 마스크 하나 제대로 분배를 못하냐, 역시 시장주의 경제를 부정하는 정권은 안된다라뇨. 한 가지만 해 주세요."
+	},
+	{
 		"created_at": "Sun Mar 01 11:57:37 +0000 2020",
 		"id": 1234049779035857000,
 		"id_str": "1234085381328359424",
