7a51510d19d9b4c843f14bcce30c4ba7a6095074
rosarux <seungwoo.ha@gmail.com>
2020-06-02 09:21:36 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 19664,
-	"created_at": "Mon Jun 01 00:21:35 +0000 2020",
+	"tweets": 19665,
+	"created_at": "Tue Jun 02 00:21:35 +0000 2020",
 	"lang": "ja"
 }
---
+++ data/js/tweets/2020_06.js
@@ -0,0 +1,84 @@
+Grailbird.data.tweets_2020_06 = 
+[
+	{
+		"created_at": "Mon Jun 01 11:27:35 +0000 2020",
+		"id": 1267067820061233200,
+		"id_str": "1267417503765946368",
+		"full_text": "의사 면허는 너무 중요해서 정부가 그 수를 엄격하게 관리해야 한다는 의사분들이 범법자 의사면허엔 아무 말 안하는거 좀 신기",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			68
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 213597009,
+			"id_str": "213597009",
+			"name": "Dit",
+			"screen_name": "618116_kr",
+			"location": "",
+			"description": "이걸 읽을 시간에 더 유익한 걸 하세요",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1127,
+			"friends_count": 366,
+			"listed_count": 20,
+			"created_at": "Tue Nov 09 09:45:04 +0000 2010",
+			"favourites_count": 621,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 138481,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/3537405485/5c704c089754ae32d8e313e08f146518_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/3537405485/5c704c089754ae32d8e313e08f146518_normal.jpeg",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 246,
+		"favorite_count": 88,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "ko",
+		"text": "의사 면허는 너무 중요해서 정부가 그 수를 엄격하게 관리해야 한다는 의사분들이 범법자 의사면허엔 아무 말 안하는거 좀 신기"
+	}
+]
