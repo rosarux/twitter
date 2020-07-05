3b9cf5e9ac72ebe424e4620db3987016e9907bd7
rosarux <seungwoo.ha@gmail.com>
2020-07-05 09:21:38 +0900

    Added 2 tweets

--- data/js/tweets/2020_07.js
+++ data/js/tweets/2020_07.js
@@ -1,6 +1,332 @@
 Grailbird.data.tweets_2020_07 = 
 [
 	{
+		"created_at": "Sat Jul 04 05:46:48 +0000 2020",
+		"id": 1279256516508307500,
+		"id_str": "1279290543965564929",
+		"full_text": "우리 세대는 486 욕할 것 없다. 486은 양심적인 세대지 적어도 민주노조 만들고 임금 올리고 부족하나마 민주개혁 했으니까. 우리 세대는 늙어서 얼마나 욕먹을까. 헬조선을 외치는 헬조선 1세대로 남을듯. 남을 무기계약직으로 끌어내리는 세대. 혐오를 유희로 만든 세대.\n\nhttps://t.co/pYl5Hh0tVS",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			175
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/pYl5Hh0tVS",
+					"expanded_url": "https://news.v.daum.net/v/20200704103033690",
+					"display_url": "news.v.daum.net/v/202007041030…",
+					"indices": [
+						152,
+						175
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1140251360,
+			"id_str": "1140251360",
+			"name": "■까만사각형 레고■ #BlackLivesMatter",
+			"screen_name": "Hadrianus753",
+			"location": "조선",
+			"description": "이것저것 씁니다 / 이것저것 혐오 안받아요 / 사회주의입니다",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1380,
+			"friends_count": 316,
+			"listed_count": 10,
+			"created_at": "Fri Feb 01 17:18:12 +0000 2013",
+			"favourites_count": 5159,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 168493,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1270638172498612226/iDCtD57b_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1270638172498612226/iDCtD57b_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1140251360/1566542009",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 371,
+		"favorite_count": 109,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "우리 세대는 486 욕할 것 없다. 486은 양심적인 세대지 적어도 민주노조 만들고 임금 올리고 부족하나마 민주개혁 했으니까. 우리 세대는 늙어서 얼마나 욕먹을까. 헬조선을 외치는 헬조선 1세대로 남을듯. 남을 무기계약직으로 끌어내리는 세대. 혐오를 유희로 만든 세대.\n\nhttps://t.co/pYl5Hh0tVS"
+	},
+	{
+		"created_at": "Sat Jul 04 02:36:52 +0000 2020",
+		"id": 1279242745270300700,
+		"id_str": "1279242745270300677",
+		"full_text": "이건 꼭 가보고 싶은데😰 https://t.co/gYkZ9udhuV",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			13
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/gYkZ9udhuV",
+					"expanded_url": "https://twitter.com/ryukyushimpo/status/1279238526597337088",
+					"display_url": "twitter.com/ryukyushimpo/s…",
+					"indices": [
+						14,
+						37
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 94077994,
+			"id_str": "94077994",
+			"name": "縄",
+			"screen_name": "kawanoboru",
+			"location": "마음 만은 언제나 오키나와",
+			"description": "沖縄戦後史を勉強してた、ソウルに住んでいる普通の会社員 오키나와 전후사를 공부한 적이 있는, 서울에 살고 있는 평범한 회사원",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 550,
+			"friends_count": 425,
+			"listed_count": 19,
+			"created_at": "Wed Dec 02 12:19:58 +0000 2009",
+			"favourites_count": 2737,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 18742,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": true,
+			"profile_background_color": "B2DFDA",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme13/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme13/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1203076836759986176/k5j8dlPA_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1203076836759986176/k5j8dlPA_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/94077994/1441838938",
+			"profile_link_color": "93A644",
+			"profile_sidebar_border_color": "EEEEEE",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1279238526597337000,
+		"quoted_status_id_str": "1279238526597337088",
+		"quoted_status_permalink": {
+			"url": "https://t.co/gYkZ9udhuV",
+			"expanded": "https://twitter.com/ryukyushimpo/status/1279238526597337088",
+			"display": "twitter.com/ryukyushimpo/s…"
+		},
+		"quoted_status": {
+			"created_at": "Sat Jul 04 02:20:06 +0000 2020",
+			"id": 1279238526597337000,
+			"id_str": "1279238526597337088",
+			"full_text": "阿波根さんが写した伊江島80点　佐喜眞美術館で「記録と記憶展」開幕　https://t.co/Kn6EhNexLE",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				57
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/Kn6EhNexLE",
+						"expanded_url": "http://ryukyushimpo.jp/news/entry-1149695.html",
+						"display_url": "ryukyushimpo.jp/news/entry-114…",
+						"indices": [
+							34,
+							57
+						]
+					}
+				]
+			},
+			"source": "<a href=\"https://ryukyushimpo.jp\" rel=\"nofollow\">ryukyu-AutoTweets</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 15746839,
+				"id_str": "15746839",
+				"name": "琉球新報",
+				"screen_name": "ryukyushimpo",
+				"location": "沖縄県那覇市",
+				"description": "沖縄の新聞社・琉球新報の公式アカウントです。▶ニュースのLINEはhttps://t.co/6ry90YboVK▶FB⇒https://t.co/9oeMxUstfq ▶知りたいに応える「りゅうちゃんねる」LINE⇒https://t.co/VQMWSzuvIR",
+				"url": "https://t.co/O5qFJJTDMI",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/O5qFJJTDMI",
+								"expanded_url": "http://ryukyushimpo.jp",
+								"display_url": "ryukyushimpo.jp",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": [
+							{
+								"url": "https://t.co/6ry90YboVK",
+								"expanded_url": "https://line.me/R/ti/p/%40oa-ryukyushimpo",
+								"display_url": "line.me/R/ti/p/%40oa-r…",
+								"indices": [
+									33,
+									56
+								]
+							},
+							{
+								"url": "https://t.co/9oeMxUstfq",
+								"expanded_url": "http://facebook.com/ryukyushimpo",
+								"display_url": "facebook.com/ryukyushimpo",
+								"indices": [
+									60,
+									83
+								]
+							},
+							{
+								"url": "https://t.co/VQMWSzuvIR",
+								"expanded_url": "http://lin.ee/bSJH5lA",
+								"display_url": "lin.ee/bSJH5lA",
+								"indices": [
+									108,
+									131
+								]
+							}
+						]
+					}
+				},
+				"protected": false,
+				"followers_count": 80355,
+				"friends_count": 6493,
+				"listed_count": 2711,
+				"created_at": "Wed Aug 06 06:12:19 +0000 2008",
+				"favourites_count": 8107,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": true,
+				"statuses_count": 122785,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "FFFCCC",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/464646031368392704/Q6HGI6qs_normal.png",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/464646031368392704/Q6HGI6qs_normal.png",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/15746839/1399601351",
+				"profile_link_color": "0084B4",
+				"profile_sidebar_border_color": "D3D920",
+				"profile_sidebar_fill_color": "FAFCC2",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 15,
+			"favorite_count": 29,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "ja"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "이건 꼭 가보고 싶은데😰 https://t.co/gYkZ9udhuV"
+	},
+	{
 		"created_at": "Thu Jul 02 11:58:42 +0000 2020",
 		"id": 1278588941327720400,
 		"id_str": "1278659360189591553",
