e5662187186b4921c458f13a16c70dda9b9da658
rosarux <seungwoo.ha@gmail.com>
2022-01-07 09:21:36 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 19708,
-	"created_at": "Thu Jan 06 00:21:35 +0000 2022",
+	"tweets": 19709,
+	"created_at": "Fri Jan 07 00:21:34 +0000 2022",
 	"lang": "ja"
 }
--- data/js/tweets/2022_01.js
+++ data/js/tweets/2022_01.js
@@ -1,6 +1,141 @@
 Grailbird.data.tweets_2022_01 = 
 [
 	{
+		"created_at": "Thu Jan 06 07:14:06 +0000 2022",
+		"id": 1478958274565521400,
+		"id_str": "1478988229261299712",
+		"full_text": "沖縄大学の次期学長に山代氏　4月に就任　https://t.co/ecA32WlPS5",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			43
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/ecA32WlPS5",
+					"expanded_url": "http://ryukyushimpo.jp/news/entry-1450046.html",
+					"display_url": "ryukyushimpo.jp/news/entry-145…",
+					"indices": [
+						20,
+						43
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://ryukyushimpo.jp\" rel=\"nofollow\">ryukyu-AutoTweets</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 15746839,
+			"id_str": "15746839",
+			"name": "琉球新報",
+			"screen_name": "ryukyushimpo",
+			"location": "沖縄県那覇市",
+			"description": "沖縄の新聞社・琉球新報の公式アカウントです。▶ニュースのLINEはhttps://t.co/6ry90YboVK▶FB⇒https://t.co/9oeMxUstfq ▶知りたいに応える「りゅうちゃんねる」LINE⇒https://t.co/VQMWSzuvIR",
+			"url": "https://t.co/O5qFJJTDMI",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/O5qFJJTDMI",
+							"expanded_url": "http://ryukyushimpo.jp",
+							"display_url": "ryukyushimpo.jp",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/6ry90YboVK",
+							"expanded_url": "https://line.me/R/ti/p/%40oa-ryukyushimpo",
+							"display_url": "line.me/R/ti/p/%40oa-r…",
+							"indices": [
+								33,
+								56
+							]
+						},
+						{
+							"url": "https://t.co/9oeMxUstfq",
+							"expanded_url": "http://facebook.com/ryukyushimpo",
+							"display_url": "facebook.com/ryukyushimpo",
+							"indices": [
+								60,
+								83
+							]
+						},
+						{
+							"url": "https://t.co/VQMWSzuvIR",
+							"expanded_url": "http://lin.ee/bSJH5lA",
+							"display_url": "lin.ee/bSJH5lA",
+							"indices": [
+								108,
+								131
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 99344,
+			"friends_count": 161,
+			"listed_count": 2883,
+			"created_at": "Wed Aug 06 06:12:19 +0000 2008",
+			"favourites_count": 8066,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 155505,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFCCC",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/464646031368392704/Q6HGI6qs_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/464646031368392704/Q6HGI6qs_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/15746839/1618390159",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "D3D920",
+			"profile_sidebar_fill_color": "FAFCC2",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 4,
+		"favorite_count": 21,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ja",
+		"text": "沖縄大学の次期学長に山代氏　4月に就任　https://t.co/ecA32WlPS5"
+	},
+	{
 		"created_at": "Wed Jan 05 21:28:05 +0000 2022",
 		"id": 1478702571464310800,
 		"id_str": "1478840752797286402",
