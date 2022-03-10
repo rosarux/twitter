52e64455cc62bf78c06fb0af307da4ccb8f18895
rosarux <seungwoo.ha@gmail.com>
2022-03-10 09:21:35 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 19725,
-	"created_at": "Mon Feb 21 00:21:34 +0000 2022",
+	"tweets": 19726,
+	"created_at": "Thu Mar 10 00:21:34 +0000 2022",
 	"lang": "ja"
 }
---
+++ data/js/tweets/2022_03.js
@@ -0,0 +1,99 @@
+Grailbird.data.tweets_2022_03 = 
+[
+	{
+		"created_at": "Wed Mar 09 04:50:59 +0000 2022",
+		"id": 1501107333463634000,
+		"id_str": "1501420259709833222",
+		"full_text": "나는 개표 방송이 안 웃겼으면 하는 생각이 있다",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			26
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 888084380945481700,
+			"id_str": "888084380945481728",
+			"name": "새벽",
+			"screen_name": "saebkkk",
+			"location": "",
+			"description": "",
+			"url": "https://t.co/qjWS5YTCQU",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/qjWS5YTCQU",
+							"expanded_url": "https://blog.naver.com/dawning_glow",
+							"display_url": "blog.naver.com/dawning_glow",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1200,
+			"friends_count": 1131,
+			"listed_count": 3,
+			"created_at": "Thu Jul 20 17:13:11 +0000 2017",
+			"favourites_count": 21711,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 62787,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1412573380914544641/YqrrhCtZ_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1412573380914544641/YqrrhCtZ_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/888084380945481728/1625619706",
+			"profile_link_color": "91D2FA",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 585,
+		"favorite_count": 845,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "ko",
+		"text": "나는 개표 방송이 안 웃겼으면 하는 생각이 있다"
+	}
+]
