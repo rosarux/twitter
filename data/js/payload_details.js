442311e5eac0b2462198efe76aa902a144724383
rosarux <seungwoo.ha@gmail.com>
2022-05-07 09:21:34 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 19730,
-	"created_at": "Fri Apr 15 00:21:34 +0000 2022",
+	"tweets": 19731,
+	"created_at": "Sat May 07 00:21:33 +0000 2022",
 	"lang": "ja"
 }
---
+++ data/js/tweets/2022_05.js
@@ -0,0 +1,156 @@
+Grailbird.data.tweets_2022_05 = 
+[
+	{
+		"created_at": "Fri May 06 23:45:07 +0000 2022",
+		"id": 1522721755730972700,
+		"id_str": "1522724168592424960",
+		"full_text": "在韓米軍機６機が嘉手納基地に飛来　目的は分からず https://t.co/pnw2VoQpQK #沖縄県 #嘉手納町 #嘉手納基地 #okinawa #沖縄",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			79
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "沖縄県",
+					"indices": [
+						49,
+						53
+					]
+				},
+				{
+					"text": "嘉手納町",
+					"indices": [
+						54,
+						59
+					]
+				},
+				{
+					"text": "嘉手納基地",
+					"indices": [
+						60,
+						66
+					]
+				},
+				{
+					"text": "okinawa",
+					"indices": [
+						67,
+						75
+					]
+				},
+				{
+					"text": "沖縄",
+					"indices": [
+						76,
+						79
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/pnw2VoQpQK",
+					"expanded_url": "https://www.okinawatimes.co.jp/articles/-/954345",
+					"display_url": "okinawatimes.co.jp/articles/-/954…",
+					"indices": [
+						25,
+						48
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://www.okinawatimes.co.jp\" rel=\"nofollow\">沖縄タイムス社TweetBot</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 118600653,
+			"id_str": "118600653",
+			"name": "沖縄タイムス",
+			"screen_name": "theokinawatimes",
+			"location": "沖縄県那覇市",
+			"description": "「沖縄タイムス」の公式アカウント。情報提供はこちら→ https://t.co/FkY73arO1y",
+			"url": "https://t.co/0pk66OxvHh",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/0pk66OxvHh",
+							"expanded_url": "http://www.okinawatimes.co.jp",
+							"display_url": "okinawatimes.co.jp",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/FkY73arO1y",
+							"expanded_url": "https://app.okinawatimes.co.jp/contact/info_form.php",
+							"display_url": "app.okinawatimes.co.jp/contact/info_f…",
+							"indices": [
+								27,
+								50
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 124323,
+			"friends_count": 145,
+			"listed_count": 3251,
+			"created_at": "Mon Mar 01 05:31:31 +0000 2010",
+			"favourites_count": 3262,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 341332,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1712719932/logo_wara_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1712719932/logo_wara_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/118600653/1640391013",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 6,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ja",
+		"text": "在韓米軍機６機が嘉手納基地に飛来　目的は分からず https://t.co/pnw2VoQpQK #沖縄県 #嘉手納町 #嘉手納基地 #okinawa #沖縄"
+	}
+]
