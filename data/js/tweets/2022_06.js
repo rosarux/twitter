96663470264bd8c254417935301a8410c66ed32b
rosarux <seungwoo.ha@gmail.com>
2022-06-03 09:21:35 +0900

    Added 1 tweets

---
+++ data/js/tweets/2022_06.js
@@ -0,0 +1,85 @@
+Grailbird.data.tweets_2022_06 = 
+[
+	{
+		"created_at": "Thu Jun 02 08:33:09 +0000 2022",
+		"id": 1532017299456086000,
+		"id_str": "1532279138106875904",
+		"full_text": "20대 남자는 설득하는게 아니라 그들의 억울 역차별 정서가 시민사회에서 수용 불가능한 혐오라는 점을 담론의 대결로 압도하여 굴복시키는 것만이 유일한 길이다.",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			87
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1496850270005112800,
+			"id_str": "1496850270005112832",
+			"name": "으앙쥬금",
+			"screen_name": "re_haxenhamme",
+			"location": "",
+			"description": "대뇌찌질(Haxenhammer)였던것",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 402,
+			"friends_count": 1002,
+			"listed_count": 2,
+			"created_at": "Thu Feb 24 14:11:34 +0000 2022",
+			"favourites_count": 6042,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 21437,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1511664719609827330/6ewSDWNW_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1511664719609827330/6ewSDWNW_normal.jpg",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 4419,
+		"favorite_count": 2271,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "ko",
+		"text": "20대 남자는 설득하는게 아니라 그들의 억울 역차별 정서가 시민사회에서 수용 불가능한 혐오라는 점을 담론의 대결로 압도하여 굴복시키는 것만이 유일한 길이다."
+	}
+]
