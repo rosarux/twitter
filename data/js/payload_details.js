a850b110084edd34c9d2ef4faac8e9e1b2215cd9
rosarux <seungwoo.ha@gmail.com>
2022-06-24 09:21:34 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 19732,
-	"created_at": "Fri Jun 03 00:21:34 +0000 2022",
+	"tweets": 19733,
+	"created_at": "Fri Jun 24 00:21:34 +0000 2022",
 	"lang": "ja"
 }
--- data/js/tweets/2022_06.js
+++ data/js/tweets/2022_06.js
@@ -1,6 +1,113 @@
 Grailbird.data.tweets_2022_06 = 
 [
 	{
+		"created_at": "Thu Jun 23 06:13:23 +0000 2022",
+		"id": 1539817055876415500,
+		"id_str": "1539854109956009984",
+		"full_text": "沖縄慰霊の日　全戦没者追悼式　玉城デニー知事平和宣言（全文） https://t.co/DRQuzy9L5h",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			54
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/DRQuzy9L5h",
+					"expanded_url": "https://mainichi.jp/20220623/k00/00m/040/028000c",
+					"display_url": "mainichi.jp/20220623/k00/0…",
+					"indices": [
+						31,
+						54
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://mainichi.jp\" rel=\"nofollow\">毎日jp</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 46282367,
+			"id_str": "46282367",
+			"name": "毎日新聞ニュース",
+			"screen_name": "mainichijpnews",
+			"location": "東京都千代田区一ツ橋",
+			"description": "毎日新聞ニュースサイトの主なニュースを掲載しています",
+			"url": "https://t.co/e2zzHYbbRb",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/e2zzHYbbRb",
+							"expanded_url": "http://mainichi.jp/",
+							"display_url": "mainichi.jp",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 443455,
+			"friends_count": 3,
+			"listed_count": 20587,
+			"created_at": "Thu Jun 11 01:39:32 +0000 2009",
+			"favourites_count": 2,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 400974,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1485531421246894082/LaZlct1j_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1485531421246894082/LaZlct1j_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/46282367/1638422859",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 117,
+		"favorite_count": 183,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ja",
+		"text": "沖縄慰霊の日　全戦没者追悼式　玉城デニー知事平和宣言（全文） https://t.co/DRQuzy9L5h"
+	},
+	{
 		"created_at": "Thu Jun 02 08:33:09 +0000 2022",
 		"id": 1532017299456086000,
 		"id_str": "1532279138106875904",
