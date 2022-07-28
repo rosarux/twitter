0ad0477c1cc092d9cd04b050d076f3f61a8ba03a
rosarux <seungwoo.ha@gmail.com>
2022-07-28 09:21:35 +0900

    Added 1 tweets

--- data/js/tweets/2022_07.js
+++ data/js/tweets/2022_07.js
@@ -1,6 +1,181 @@
 Grailbird.data.tweets_2022_07 = 
 [
 	{
+		"created_at": "Wed Jul 27 04:22:46 +0000 2022",
+		"id": 1552092148300148700,
+		"id_str": "1552147460822183936",
+		"full_text": "맘에들었습니다 보러갈게요 https://t.co/t0xyYVHgyb",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			13
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1552092139055894500,
+					"id_str": "1552092139055894529",
+					"indices": [
+						14,
+						37
+					],
+					"media_url": "http://pbs.twimg.com/media/FYojk78aUAEQvvK.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FYojk78aUAEQvvK.jpg",
+					"url": "https://t.co/t0xyYVHgyb",
+					"display_url": "pic.twitter.com/t0xyYVHgyb",
+					"expanded_url": "https://twitter.com/franklymadear/status/1552092148300148736/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 398,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1200,
+							"h": 703,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 703,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1552092139055894500,
+					"id_str": "1552092139055894529",
+					"indices": [
+						14,
+						37
+					],
+					"media_url": "http://pbs.twimg.com/media/FYojk78aUAEQvvK.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FYojk78aUAEQvvK.jpg",
+					"url": "https://t.co/t0xyYVHgyb",
+					"display_url": "pic.twitter.com/t0xyYVHgyb",
+					"expanded_url": "https://twitter.com/franklymadear/status/1552092148300148736/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 398,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1200,
+							"h": 703,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 703,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1491606850911223800,
+			"id_str": "1491606850911223810",
+			"name": "odd",
+			"screen_name": "franklymadear",
+			"location": "",
+			"description": "제철영화 오마카세",
+			"url": "https://t.co/dJtndtT73m",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/dJtndtT73m",
+							"expanded_url": "https://peing.net/ko/franklymadear",
+							"display_url": "peing.net/ko/franklymade…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 15069,
+			"friends_count": 164,
+			"listed_count": 39,
+			"created_at": "Thu Feb 10 02:56:08 +0000 2022",
+			"favourites_count": 8422,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 11410,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1498859939015839744/5X2v3KVB_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1498859939015839744/5X2v3KVB_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1491606850911223810/1657691209",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 7518,
+		"favorite_count": 4122,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "맘에들었습니다 보러갈게요 https://t.co/t0xyYVHgyb"
+	},
+	{
 		"created_at": "Sat Jul 09 12:19:27 +0000 2022",
 		"id": 1545744438575542300,
 		"id_str": "1545744438575542272",
