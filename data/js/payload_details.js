dbff370312cc94fef9d90b9878a4f7a8d7049c85
rosarux <seungwoo.ha@gmail.com>
2022-09-24 09:21:35 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 19741,
-	"created_at": "Sat Sep 10 00:21:34 +0000 2022",
+	"tweets": 19742,
+	"created_at": "Sat Sep 24 00:21:34 +0000 2022",
 	"lang": "ja"
 }
--- data/js/tweets/2022_09.js
+++ data/js/tweets/2022_09.js
@@ -1,6 +1,123 @@
 Grailbird.data.tweets_2022_09 = 
 [
 	{
+		"created_at": "Fri Sep 23 00:46:41 +0000 2022",
+		"id": 1573109589775904800,
+		"id_str": "1573111575812407296",
+		"full_text": "米・フィリピン首脳、南シナ海情勢を協議　初の対面会談\nhttps://t.co/kGsR4gSpsj",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			50
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/kGsR4gSpsj",
+					"expanded_url": "https://www.nikkei.com/article/DGXZQOGN230680T20C22A9000000/?n_cid=SNSTW001&n_tw=1663893511",
+					"display_url": "nikkei.com/article/DGXZQO…",
+					"indices": [
+						27,
+						50
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://www.nikkei.com/\" rel=\"nofollow\">日本経済新聞 電子版</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 169480493,
+			"id_str": "169480493",
+			"name": "日本経済新聞 電子版（日経電子版）",
+			"screen_name": "nikkei",
+			"location": "",
+			"description": "日経電子版の公式アカウントです。トップページで報じたニュースを中心に注目記事を24時間発信。記事は会員向け（登録無料）を含みます。テーマ別や記者個人のアカウントは https://t.co/3H13iCnaMC をご覧ください。",
+			"url": "https://t.co/Iajvc1gZ1K",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Iajvc1gZ1K",
+							"expanded_url": "http://www.nikkei.com",
+							"display_url": "nikkei.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/3H13iCnaMC",
+							"expanded_url": "https://www.nikkei.com/edit/sns/?n_cid=DSSCN001",
+							"display_url": "nikkei.com/edit/sns/?n_ci…",
+							"indices": [
+								82,
+								105
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 3682066,
+			"friends_count": 70,
+			"listed_count": 43408,
+			"created_at": "Thu Jul 22 12:58:30 +0000 2010",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 358826,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": true,
+			"profile_background_color": "0F3675",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme7/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme7/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1161879315933351936/CQbpby6s_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1161879315933351936/CQbpby6s_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/169480493/1572577650",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "DFDFDF",
+			"profile_sidebar_fill_color": "F3F3F3",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 13,
+		"favorite_count": 49,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ja",
+		"text": "米・フィリピン首脳、南シナ海情勢を協議　初の対面会談\nhttps://t.co/kGsR4gSpsj"
+	},
+	{
 		"created_at": "Fri Sep 09 02:55:57 +0000 2022",
 		"id": 1568070676875194400,
 		"id_str": "1568070676875194368",
