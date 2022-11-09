50a0181632565bcb1e602f5f1a353bb8942bbdd0
rosarux <seungwoo.ha@gmail.com>
2022-11-09 09:21:35 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 19744,
-	"created_at": "Sun Oct 23 00:21:34 +0000 2022",
+	"tweets": 19745,
+	"created_at": "Wed Nov 09 00:21:34 +0000 2022",
 	"lang": "ja"
 }
---
+++ data/js/tweets/2022_11.js
@@ -0,0 +1,199 @@
+Grailbird.data.tweets_2022_11 = 
+[
+	{
+		"created_at": "Tue Nov 08 12:25:50 +0000 2022",
+		"id": 1589531706763382800,
+		"id_str": "1589957363242774528",
+		"full_text": "아니 예산 깎은게 민주당이냐곸ㅋㅋㅋ 7000억 복구한게 민주당이면 이 상황에서 민주당을 도와 나머지 예산도 복구할 수 있도록 노력하겠습니다가 나오는게 정상 아니냐고요 뭔 오십보백보야 예산 삭감하는놈 따로 있고 복구하는놈 따로있는데 그래서 정의당은 뭐하시는데요 뭘 할 수 있기는 함? https://t.co/eI1oCOGwar",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			157
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/eI1oCOGwar",
+					"expanded_url": "https://twitter.com/sangjungsim/status/1589441797809672193",
+					"display_url": "twitter.com/sangjungsim/st…",
+					"indices": [
+						158,
+						181
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 881040201438511100,
+			"id_str": "881040201438511106",
+			"name": "은시엔",
+			"screen_name": "xian_wkqejr",
+			"location": "더불어민주당/민주대륙과채연합 딸기지부",
+			"description": "난 몰랐어 내 맘이 이리 다채로운지\n잡덕/드덕/중년헤테로러버/김남길/김상경/김현주 그 외 다수/헌트/환혼/트롤리 대기 중\n●언팔/블언블 편하게 해주세용●\n비계팔은 맞팔or블언블",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 202,
+			"friends_count": 817,
+			"listed_count": 2,
+			"created_at": "Sat Jul 01 06:42:07 +0000 2017",
+			"favourites_count": 4054,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 6764,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1576768994971115520/iePRjDi0_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1576768994971115520/iePRjDi0_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/881040201438511106/1667806837",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1589441797809672200,
+		"quoted_status_id_str": "1589441797809672193",
+		"quoted_status_permalink": {
+			"url": "https://t.co/eI1oCOGwar",
+			"expanded": "https://twitter.com/sangjungsim/status/1589441797809672193",
+			"display": "twitter.com/sangjungsim/st…"
+		},
+		"quoted_status": {
+			"created_at": "Mon Nov 07 02:17:09 +0000 2022",
+			"id": 1589441797809672200,
+			"id_str": "1589441797809672193",
+			"full_text": "한편, 민주당은 국민임대와 영구임대 건설 감소분 6,993억 원만 복구하겠다고 선언했습니다. 5조 7천 삭감과 5조 삭감의 차이? 결국 민주당의 민생은 국민의힘과 오십 보 백 보입니다. 정말 민생을 생각한다면, 전액을 복구해야 합니다. 민생을 외치면서 국민의 주거안정을 외면하는 것은 사기입니다.",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				165
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": []
+			},
+			"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+			"in_reply_to_status_id": 1589441795465023500,
+			"in_reply_to_status_id_str": "1589441795465023488",
+			"in_reply_to_user_id": 27821557,
+			"in_reply_to_user_id_str": "27821557",
+			"in_reply_to_screen_name": "sangjungsim",
+			"user": {
+				"id": 27821557,
+				"id_str": "27821557",
+				"name": "심상정",
+				"screen_name": "sangjungsim",
+				"location": "Korea",
+				"description": "#심블리 가 여러분의 자부심이 되겠습니다. 정의당 국회의원 심상정입니다. 문의: 713sim@naver.com",
+				"url": "https://t.co/BwKs5UvExZ",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/BwKs5UvExZ",
+								"expanded_url": "https://blog.naver.com/713sim",
+								"display_url": "blog.naver.com/713sim",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 731743,
+				"friends_count": 72061,
+				"listed_count": 6440,
+				"created_at": "Tue Mar 31 05:29:28 +0000 2009",
+				"favourites_count": 145,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": true,
+				"statuses_count": 10624,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "517180",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme10/bg.gif",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme10/bg.gif",
+				"profile_background_tile": true,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1515865763852460034/WQWjBL4Y_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1515865763852460034/WQWjBL4Y_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/27821557/1667785195",
+				"profile_link_color": "F58EA8",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "DDFFCC",
+				"profile_text_color": "333333",
+				"profile_use_background_image": false,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none",
+				"withheld_in_countries": []
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 10,
+			"favorite_count": 17,
+			"favorited": false,
+			"retweeted": false,
+			"lang": "ko"
+		},
+		"retweet_count": 1234,
+		"favorite_count": 317,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "아니 예산 깎은게 민주당이냐곸ㅋㅋㅋ 7000억 복구한게 민주당이면 이 상황에서 민주당을 도와 나머지 예산도 복구할 수 있도록 노력하겠습니다가 나오는게 정상 아니냐고요 뭔 오십보백보야 예산 삭감하는놈 따로 있고 복구하는놈 따로있는데 그래서 정의당은 뭐하시는데요 뭘 할 수 있기는 함? https://t.co/eI1oCOGwar"
+	}
+]
