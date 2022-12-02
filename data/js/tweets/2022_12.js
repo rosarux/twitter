542e0c97cb31700f7ddb338680b76111c0e58abc
rosarux <seungwoo.ha@gmail.com>
2022-12-02 09:21:35 +0900

    Added 1 tweets

---
+++ data/js/tweets/2022_12.js
@@ -0,0 +1,173 @@
+Grailbird.data.tweets_2022_12 = 
+[
+	{
+		"created_at": "Thu Dec 01 21:08:56 +0000 2022",
+		"id": 1598423929328402400,
+		"id_str": "1598423929328402432",
+		"full_text": "트위터에 가입한 날이 언제인지 기억하시나요? 트위터는 기억한답니다! #MyTwitterAnniversary https://t.co/EWJxOLl7HC",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			59
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "MyTwitterAnniversary",
+					"indices": [
+						38,
+						59
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1598423922977841200,
+					"id_str": "1598423922977841152",
+					"indices": [
+						60,
+						83
+					],
+					"media_url": "http://pbs.twimg.com/media/Fi6-FtGUoAArayB.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Fi6-FtGUoAArayB.jpg",
+					"url": "https://t.co/EWJxOLl7HC",
+					"display_url": "pic.twitter.com/EWJxOLl7HC",
+					"expanded_url": "https://twitter.com/kawanoboru/status/1598423929328402432/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1598423922977841200,
+					"id_str": "1598423922977841152",
+					"indices": [
+						60,
+						83
+					],
+					"media_url": "http://pbs.twimg.com/media/Fi6-FtGUoAArayB.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Fi6-FtGUoAArayB.jpg",
+					"url": "https://t.co/EWJxOLl7HC",
+					"display_url": "pic.twitter.com/EWJxOLl7HC",
+					"expanded_url": "https://twitter.com/kawanoboru/status/1598423929328402432/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 94077994,
+			"id_str": "94077994",
+			"name": "縄",
+			"screen_name": "kawanoboru",
+			"location": "마음 만은 언제나 오키나와",
+			"description": "沖縄戦後史を勉強してた、ソウルに住んでいる普通の会社員 오키나와 전후사를 공부한 적이 있는, 서울에 살고 있는 평범한 회사원",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 524,
+			"friends_count": 407,
+			"listed_count": 20,
+			"created_at": "Wed Dec 02 12:19:58 +0000 2009",
+			"favourites_count": 2474,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 18600,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": true,
+			"profile_background_color": "B2DFDA",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme13/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme13/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1501684701366714370/dQNhcMVw_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1501684701366714370/dQNhcMVw_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/94077994/1646864518",
+			"profile_link_color": "93A644",
+			"profile_sidebar_border_color": "EEEEEE",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "트위터에 가입한 날이 언제인지 기억하시나요? 트위터는 기억한답니다! #MyTwitterAnniversary https://t.co/EWJxOLl7HC"
+	}
+]
