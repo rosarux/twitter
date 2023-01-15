e6c6a68a0e0680aa4fa4ec054723aab9d66f2006
rosarux <seungwoo.ha@gmail.com>
2023-01-15 09:21:35 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 19748,
-	"created_at": "Sun Jan 01 00:21:34 +0000 2023",
+	"tweets": 19749,
+	"created_at": "Sun Jan 15 00:21:34 +0000 2023",
 	"lang": "ja"
 }
---
+++ data/js/tweets/2023_01.js
@@ -0,0 +1,219 @@
+Grailbird.data.tweets_2023_01 = 
+[
+	{
+		"created_at": "Sat Jan 14 21:13:53 +0000 2023",
+		"id": 1613551010139758600,
+		"id_str": "1614370238514200576",
+		"full_text": "鳩山元首相が158人が犠牲になった梨泰院の事故現場を訪れ、慰霊、献花、記帳を行った。日韓関係は未だ不安定だが、国家主導ではなく民間同士でも矜持をもって交流すれば、解決不可能なことはない。我々も卑屈になることはなく、堂々と「倭徳山精神」の歴史に倣い、未来志向の日韓関係を目指すべきだ。 https://t.co/tCJHkk9eXN",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			141
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1613550951767638000,
+					"id_str": "1613550951767638016",
+					"indices": [
+						142,
+						165
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1613550951767638016/pu/img/xhvLM7gXRSKYjBzW.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1613550951767638016/pu/img/xhvLM7gXRSKYjBzW.jpg",
+					"url": "https://t.co/tCJHkk9eXN",
+					"display_url": "pic.twitter.com/tCJHkk9eXN",
+					"expanded_url": "https://twitter.com/issuikai_jp/status/1613551010139758593/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 540,
+							"h": 960,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 383,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 540,
+							"h": 960,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1613550951767638000,
+					"id_str": "1613550951767638016",
+					"indices": [
+						142,
+						165
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1613550951767638016/pu/img/xhvLM7gXRSKYjBzW.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1613550951767638016/pu/img/xhvLM7gXRSKYjBzW.jpg",
+					"url": "https://t.co/tCJHkk9eXN",
+					"display_url": "pic.twitter.com/tCJHkk9eXN",
+					"expanded_url": "https://twitter.com/issuikai_jp/status/1613551010139758593/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 540,
+							"h": 960,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 383,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 540,
+							"h": 960,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							9,
+							16
+						],
+						"duration_millis": 30750,
+						"variants": [
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1613550951767638016/pu/pl/ZvaKY5JVAIoTudGi.m3u8?tag=12&container=fmp4"
+							},
+							{
+								"bitrate": 950000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1613550951767638016/pu/vid/480x852/0LacpyOdI1_7uEN5.mp4?tag=12"
+							},
+							{
+								"bitrate": 2176000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1613550951767638016/pu/vid/540x960/WuyDwAZoBniJ5p4A.mp4?tag=12"
+							},
+							{
+								"bitrate": 632000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1613550951767638016/pu/vid/320x568/PdFvIvr9gWRH6Oph.mp4?tag=12"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 437135264,
+			"id_str": "437135264",
+			"name": "一水会",
+			"screen_name": "issuikai_jp",
+			"location": "東京都",
+			"description": "日本 愛国者団体･一水会の公式アカウントです。一水会の主張、活動などを速報でお伝えします。昭和50年8月創刊の一水会機関紙･月刊「レコンキスタ」は毎月1日発行です。\nご購読のお申込は\nhttps://t.co/WIbfw49ubn\nより",
+			"url": "https://t.co/aX9yu79oBV",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/aX9yu79oBV",
+							"expanded_url": "http://issuikai.jp",
+							"display_url": "issuikai.jp",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/WIbfw49ubn",
+							"expanded_url": "http://www.issuikai.jp/reconquista2.html",
+							"display_url": "issuikai.jp/reconquista2.h…",
+							"indices": [
+								92,
+								115
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 45259,
+			"friends_count": 10,
+			"listed_count": 540,
+			"created_at": "Thu Dec 15 01:50:33 +0000 2011",
+			"favourites_count": 22,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 6039,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1693885634/fla_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1693885634/fla_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/437135264/1638352932",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 263,
+		"favorite_count": 1296,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ja",
+		"text": "鳩山元首相が158人が犠牲になった梨泰院の事故現場を訪れ、慰霊、献花、記帳を行った。日韓関係は未だ不安定だが、国家主導ではなく民間同士でも矜持をもって交流すれば、解決不可能なことはない。我々も卑屈になることはなく、堂々と「倭徳山精神」の歴史に倣い、未来志向の日韓関係を目指すべきだ。 https://t.co/tCJHkk9eXN"
+	}
+]
